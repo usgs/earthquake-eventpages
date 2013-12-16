ec43ead485f0e7d05b8667fdb94760c85afa03e6
Hal Simpson <hasimpson@usgs.gov>
2013-12-16 16:20:50 -0700

    Added GraphPage code, and moved similiar code for Graph and Map into CreateTabListData.js

--- src/htdocs/modules/dyfi/0-0-1/js/DYFIGraphPage.js
+++ src/htdocs/modules/dyfi/0-0-1/js/DYFIGraphPage.js
@@ -1,10 +1,16 @@
 /* global define */
 define([
 	'util/Util',
-	'base/EventModulePage'
+	'base/EventModulePage',
+	'tablist/Tablist',
+	'tablist/ImageList',
+	'./CreateTabListData'
 ], function (
 	Util,
-	EventModulePage
+	EventModulePage,
+	TabList,
+	ImageList,
+	CreateTabListData
 ) {
 	'use strict';
 
@@ -13,11 +19,42 @@ define([
 		hash: 'graphs'
 	};
 
+	var GRAPHIMAGES = [
+		{
+			title:'Intensity vs. Distance',
+			suffix:'_plot_atten.jpg'
+		},
+		{
+			title:'Response Vs. Time',
+			suffix:'_plot_numresp.jpg'
+		}
+	];
+
 	var DYFIGraphPage = function (options) {
 		options = Util.extend({}, DEFAULTS, options || {});
 		EventModulePage.call(this, options);
 	};
 	DYFIGraphPage.prototype = Object.create(EventModulePage.prototype);
+
+	DYFIGraphPage.prototype._setContentMarkup = function () {
+		var products = this._event.properties.products,
+		    dyfi;
+		if (!products.dyfi) {
+			return;
+		}
+
+		dyfi = products.dyfi[0];
+		new ImageList({
+			el: this._content.appendChild(document.createElement('div')),
+			tabPosition: 'right',
+			tabs: new CreateTabListData(
+				{contents:dyfi.contents,
+				 eventId:dyfi.code,
+				 dataObject:GRAPHIMAGES
+				})
+		});
+
+	};
 
 	return DYFIGraphPage;
 });
