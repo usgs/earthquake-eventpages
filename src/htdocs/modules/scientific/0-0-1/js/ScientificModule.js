2c334c17ee029cfe27a0fa8f9ec1e43c6aefca66
Jeremy Fee <jmfee@usgs.gov>
2014-03-13 11:28:13 -0600

    Configured product types on scientific module pages

--- src/htdocs/modules/scientific/0-0-1/js/ScientificModule.js
+++ src/htdocs/modules/scientific/0-0-1/js/ScientificModule.js
@@ -21,35 +21,45 @@ define([
 				options: {
 					title: 'Summary',
 					hash: 'summary'
-				}
+				},
+				productTypes: [
+					'origin',
+					'phase-data',
+					'moment-tensor',
+					'focal-mechanism',
+					'finite-fault']
 			},
 			{
 				className: 'scientific/HypocenterPage',
 				options: {
 					title: 'Hypocenter',
 					hash: 'hypocenter'
-				}
+				},
+				productTypes: ['origin', 'phase-data']
 			},
 			{
 				className: 'scientific/MomentTensorPage',
 				options: {
 					title: 'Moment Tensor',
 					hash: 'tensor'
-				}
+				},
+				productTypes: ['moment-tensor']
 			},
 			{
 				className: 'scientific/FocalMechanismPage',
 				options: {
 					title: 'Focal Mechanism',
 					hash: 'mechanism'
-				}
+				},
+				productTypes: ['focal-mechanism']
 			},
 			{
 				className: 'scientific/FiniteFaultPage',
 				options: {
 					title: 'Finite Fault',
 					hash: 'finitefault'
-				}
+				},
+				productTypes: ['finite-fault']
 			}
 		]
 	};
