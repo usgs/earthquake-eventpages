57190d568265effce85e0d9115130193cf4b9994
ehunter-usgs <ehunter@usgs.gov>
2014-04-04 14:37:12 -0600

    Create details page that displays shakemap products in a tabbed layout.

--- src/htdocs/modules/shakemap/0-0-1/js/ShakemapDetailsPage.js
+++ src/htdocs/modules/shakemap/0-0-1/js/ShakemapDetailsPage.js
@@ -60,6 +60,14 @@ define([
 		'N': 'Not in list of known stations'
 	};
 
+	var _translateMmi = function (mmi) {
+		var mmiArray = ['I', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII',
+				'IX', 'X', 'XI', 'XII'];
+		mmi = Math.round(mmi);
+
+		return mmiArray[mmi] || '';
+	};
+
 	var _sortByDistance = function (a, b) {
 		return parseFloat(a.dist, 10) - parseFloat(b.dist, 10);
 	};
@@ -89,10 +97,12 @@ define([
 		}
 
 		shakemap = this._shakemap = products.shakemap[0];
+
+		// Build TabList with all of the shakemap images
 		this._tablist = new TabList({
 			el: this._content.appendChild(document.createElement('div')),
 			tabPosition: 'right',
-			header: shakemap.code,
+			header: shakemap.code, // TODO, what should this be?
 			tabs: TabListUtil.CreateTabListData(
 				{
 					contents: shakemap.contents,
@@ -101,96 +111,204 @@ define([
 				})
 		});
 
-		// add station list
+		// Add station list to TabList
 		this._addStationList();
 	};
 
+	/**
+	 * Build a list of stations from stationlist.xml
+	 */
 	ShakemapDetailsPage.prototype._addStationList = function () {
 		var title = 'Station List',
-		    _this = this,
-		    stationList;
+		    _this = this;
 
+		// Add tab with station list
 		this._tablist.addTab({
 			'title': title,
 			'content': function () {
 				var container = document.createElement('div');
+				container.className = 'stations';
+				container.innerHTML = '<p>Loading station list data from XML, please wait...</p>';
 
 				_this._getStationData(function (content) {
-					var startTime = new Date().getTime();
+					// add station list content
 					container.innerHTML = content;
-					var endTime = new Date().getTime();
-					console.log('innerHTML completed ' + (endTime - startTime));
-
-					// add click event handling
-					//stationList = container.querySelector('table.stations');
+					// add click event to toggle details for stations
 					container.addEventListener('click', _this._toggleDetails.bind(_this));
-
 				});
 
+				// return panel content
 				return container;
 			}
 		});
 	};
 
-	ShakemapDetailsPage.prototype._toggleDetails = function (e) {
-		var tr = e.target.parentElement,
-		    className,
-		    container,
-		    details,
-		    newRow;
-
-		if (tr.nodeName === 'TR' && tr.classList.contains('toggle')) {
-			className = 'details details-' + tr.getAttribute('data-id');
-			container = tr.parentNode;
-			details = this._buildStationDetails(tr.getAttribute('data-id'));
-			newRow = document.createElement('tr');
-			newRow.className = className;
-			newRow.innerHTML = details;
-			// var panel = document.querySelector('.event-module-content');
-			// panel.insertBefore(newRow, panel.firstChild);
-			if(tr.nextSibling) {
-				container.insertBefore(newRow, tr.nextSibling);
-			} else {
-				container.appendChild(newRow);
-			}
-
-			//details.classList.toggle('hidden');
-		}
-
-		console.log(details);
-	};
-
 	ShakemapDetailsPage.prototype._getStationData = function (callback) {
 		var file = this._shakemap.contents[STATION_LIST.suffix],
-		    _this = this,
-		    content;
+		    _this = this;
 
-		var startTime = new Date().getTime();
-
+		// get station content and build the station list
 		Xhr.ajax({
 			url: file.url,
 			success: function (data, xhr) {
-				var endTime = new Date().getTime();
-				console.log('download complete in: ' + (endTime-startTime));
-				var stationArray = _this._buildStationArray(xhr.responseXML);
-
-				startTime = new Date().getTime();
-				var stationList = _this._buildStationList(stationArray);
-				endTime = new Date().getTime();
-				console.log('stationList complete in: ' + (endTime-startTime));
-
-				callback(stationList);
-
+				callback(_this._buildStationList(_this._buildStationArray(
+						xhr.responseXML)));
 			},
 			error: function () {
 				callback('<p class="shakemap-error">Error: Unable to retreive the station list.</p>');
 			}
 		});
-
-		return content;
 	};
 
-	ShakemapDetailsPage.prototype.xmlToJson = function (xml) {
+	ShakemapDetailsPage.prototype._buildStationArray = function (xml) {
+		var data = this._xmlToJson(xml),
+		    shakemapData = data['shakemap-data'][1],
+		    stations = this._stations = shakemapData.stationlist.station;
+
+		// sort by distance
+		stations.sort(_sortByDistance);
+
+		return stations;
+	};
+
+	ShakemapDetailsPage.prototype._buildStationList = function (data) {
+		var stations = [],
+		    station, acc, vel, dist, components, romanNumeral, title;
+
+		for (var i = 0; i < data.length; i++) {
+			station = data[i];
+			components = station.comp;
+
+			acc = this._findMaxValue(components, 'acc');
+			vel = this._findMaxValue(components, 'vel');
+
+			// check for NaN
+			if (typeof vel !== 'string') {
+				vel = vel.toFixed(3);
+			}
+
+			// check for NaN
+			if (typeof acc !== 'string') {
+				acc = acc.toFixed(3);
+			}
+
+			if (typeof station.dist === 'string') {
+				dist = parseFloat(station.dist, 10);
+			}
+
+			dist = dist.toFixed(1);
+
+			romanNumeral = _translateMmi(station.intensity);
+
+			// Do not repeat the zip code if it's already part of the name
+			if (station.name.indexOf('ZIP Code') === -1) {
+				title = '<em>' + station.code + '</em>' + station.name;
+			} else {
+				title = station.name;
+			}
+
+			stations.push([
+				'<div class="toggle" data-id="', i ,'">',
+					'<h3>', title, '</h3>',
+					'<ul class="summary">',
+						'<li class="mmi mmi', romanNumeral, '">',
+							'<span>', station.intensity, '</span>',
+							'<abbr title="Modified Mercalli Intensity">MMI</abbr>',
+						'</li>',
+						'<li>',
+							'<span>', vel ,'</span>',
+							'<abbr title="Maximum Horizontal Peak Ground Velocity">pgv</abbr>',
+						'</li>',
+						'<li>',
+							'<span>', acc ,'</span>',
+							'<abbr title="Maximum Horizontal Peak Ground Acceleration">pga</abbr>',
+						'</li>',
+						'<li>',
+							'<span>', dist ,'</span>',
+							'<abbr title="Distance from Epicenter">dist</abbr>',
+						'</li>',
+					'</ul>',
+					'<a class="expand" data-id="', i ,'">Details</a>',
+				'</div>'
+			].join(''));
+		}
+
+		return stations.join('');
+	};
+
+
+	ShakemapDetailsPage.prototype._buildStationDetails = function (index) {
+		var station = this._stations[index],
+		    components = station.comp;
+
+		// check for null index id
+		if (!index) {
+			return;
+		}
+
+		return [
+				'<dl>',
+					'<dt>Type: </dt>',
+					'<dd>', (station.insttype === '') ? '--' : station.insttype ,'</dd>',
+					'<dt>Location: </dt>',
+					'<dd>(', station.lat, ', ', station.lon ,')</dd>',
+					'<dt>Source: </dt>',
+					'<dd>', station.source ,'</dd>',
+				'</dl>',
+				this._buildComponentDetails(components),
+			].join('');
+	};
+
+	ShakemapDetailsPage.prototype._buildComponentDetails = function (components) {
+		var componentsMarkup = [],
+		    component;
+
+		// check for null
+		if (!components) {
+			return;
+		}
+
+		// When one record exists components is an object not an array
+		if(components && !components.length) {
+			components = [components];
+		}
+
+		for(var i = 0; i < components.length; i++) {
+			component = components[i];
+
+			componentsMarkup.push([
+				'<tr>',
+					'<td>', component.name ,'</td>',
+					this._buildTableCell(component.acc),
+					this._buildTableCell(component.vel),
+					this._buildTableCell(component.psa03),
+					this._buildTableCell(component.psa10),
+					this._buildTableCell(component.psa30),
+				'</tr>'
+			].join(''));
+		}
+
+		return [
+			'<table class="responsive components">',
+				'<thead>',
+					'<tr>',
+						'<th>name</th>',
+						'<th>acc</th>',
+						'<th>vel</th>',
+						'<th>psa03</th>',
+						'<th>psa10</th>',
+						'<th>psa30</th>',
+					'</tr>',
+				'</thead>',
+				'<tbody>',
+						componentsMarkup.join(''),
+				'</tbody>',
+			'</table>'
+		].join('');
+	};
+
+
+	ShakemapDetailsPage.prototype._xmlToJson = function (xml) {
 		// based on http://davidwalsh.name/convert-xml-json
 		var obj = {},
 				    children = [],
@@ -220,7 +338,7 @@ define([
 			for(i = 0, len = nodes.length; i < len; i++) {
 				node = nodes.item(i);
 				nodeName = node.nodeName;
-				nodeValue = this.xmlToJson(node);
+				nodeValue = this._xmlToJson(node);
 				children.push(nodeValue);
 				if (typeof(obj[nodeName]) === 'undefined') {
 					obj[nodeName] = nodeValue;
@@ -243,89 +361,33 @@ define([
 		return obj;
 	};
 
-	ShakemapDetailsPage.prototype._buildStationArray = function (xml) {
-		var startTime = new Date().getTime(),
-		    data = this.xmlToJson(xml),
-		    endTime = new Date().getTime(),
-		    shakemapData = data['shakemap-data'][1],
-		    stations = this._stations = shakemapData.stationlist.station;
 
+	ShakemapDetailsPage.prototype._toggleDetails = function (e) {
+		var target = e.target,
+		    container = e.target.parentNode,
+		    className,
+		    details,
+		    detailSection,
+		    newSection;
 
-				console.log('parse complete in: ' + (endTime-startTime));
-
-		// sort by distance
-		startTime = new Date().getTime();
-		stations.sort(_sortByDistance);
-		endTime = new Date().getTime();
-		console.log('sort complete in: ' + (endTime-startTime));
-
-		return stations;
-	};
-
-	ShakemapDetailsPage.prototype._buildStationList = function (data) {
-		var markup = [],
-		    stations = [],
-		    station, acc, vel, dist, components;
-
-		for (var i = 0; i < data.length; i++) {
-			station = data[i];
-			components = station.comp;
-
-			acc = this._findMaxValue(components, 'acc');
-			vel = this._findMaxValue(components, 'vel');
-
-			// check for NaN
-			if (typeof vel !== 'string') {
-				vel = vel.toFixed(3);
+		if (target.nodeName === 'A' && target.classList.contains('expand')) {
+			// after creating the section, toggle the details on click
+			detailSection = container.querySelector('.details');
+			if (detailSection) {
+				target.classList.toggle('open');
+				detailSection.classList.toggle('details-hidden');
+				return;
 			}
 
-			// check for NaN
-			if (typeof acc !== 'string') {
-				acc = acc.toFixed(3);
-			}
+			className = 'details details-' + target.getAttribute('data-id');
+			details = this._buildStationDetails(target.getAttribute('data-id'));
+			newSection = document.createElement('div');
+			newSection.className = className;
+			newSection.innerHTML = details;
 
-			if (typeof station.dist === 'string') {
-				dist = parseFloat(station.dist, 10);
-			}
-
-			dist = dist.toFixed(1);
-
-
-			stations.push([
-				'<tr class="toggle" data-id="', i ,'">',
-					'<td><abbr title="', station.name ,'">', station.code,'</abbr></td>',
-					//'<td>', station.name, '</td>',
-					'<td>', dist, ' km</td>',
-					'<td>', acc, '</td>', // max acc
-					'<td>', vel, '</td>',
-					'<td>', station.intensity, '</td>',
-				'</tr>'
-				// '<tr class="hidden details details-', codeCSS ,'">',
-				// 	//'<td colspan="6">',
-				// 	'<td colspan="5">',
-				// 		this._buildStationDetails(station),
-				// 	'</td>',
-				// '</tr>'
-			].join(''));
+			target.classList.toggle('open');
+			container.appendChild(newSection);
 		}
-
-		markup = [
-			'<table class="responsive stations">',
-				'<thead>',
-					'<th>Name</th>',
-					//'<th>Name</th>',
-					'<th>Distance</th>',
-					'<th>PGA</th>',
-					'<th>PGV</th>',
-					'<th>MMI</th>',
-				'</thead>',
-				'<tbody>',
-					stations.join(''),
-				'</tbody>',
-			'</table>'
-		];
-
-		return markup.join('');
 	};
 
 	ShakemapDetailsPage.prototype._findMaxValue = function (array, value) {
@@ -355,97 +417,17 @@ define([
 		return Math.max.apply(null, values);
 	};
 
-	ShakemapDetailsPage.prototype._buildStationDetails = function (index) {
-		var markup = [],
-		    station = this._stations[index],
-		    components = station.comp;
-
-		markup.push([
-			'<td colspan="5">',
-				'<strong>', station.name ,' (', station.code ,')</strong>',
-				'<dl>',
-					'<dt>Type: </dt>',
-					'<dd>', station.insttype ,'</dd>',
-					'<dt>Location: </dt>',
-					'<dd>(', station.lat, ', ', station.lon ,')</dd>',
-					'<dt>Distance: </dt>',
-					'<dd>', station.dist ,'</dd>',
-					'<dt>Source: </dt>',
-					'<dd>', station.source ,'</dd>',
-					'<dt>Intensity: </dt>',
-					'<dd>', station.intensity ,'</dd>',
-				'</dl>',
-				this._buildComponentDetails(components),
-			'</td>'
-		].join(''));
-
-		return markup.join('');
-	};
-
-	ShakemapDetailsPage.prototype._buildComponentDetails = function (components) {
-		var markup = [],
-		    componentsMarkup = [],
-		    component;
-
-		if(components && !components.length) {
-			components = [components];
-		}
-
-		for(var i = 0; i < components.length; i++) {
-			component = components[i];
-
-			componentsMarkup.push([
-				'<tr>',
-					'<td>', component.name ,'</td>',
-					this._buildTableCell(component.acc),
-					this._buildTableCell(component.vel),
-					this._buildTableCell(component.psa03),
-					this._buildTableCell(component.psa10),
-					this._buildTableCell(component.psa30),
-				'</tr>'
-			].join(''));
-		}
-
-		markup.push([
-			'<table class="responsive components">',
-				'<thead>',
-					'<tr>',
-						'<th>name</th>',
-						'<th>acc</th>',
-						'<th>vel</th>',
-						'<th>psa03</th>',
-						'<th>psa10</th>',
-						'<th>psa30</th>',
-					'</tr>',
-				'</thead>',
-				'<tbody>',
-						componentsMarkup.join(''),
-				'</tbody>',
-			'</table>'
-		].join(''));
-
-		return markup.join('');
-	};
-
-
 	ShakemapDetailsPage.prototype._buildTableCell = function (data) {
 		var td;
 
+		// Add flag class for all non-zero flags
 		if (data) {
-			if (data.flag !== '0') {
 				td = [
-					'<td class="flag">',
+					(data.flag !== '0') ? '<td class="flag">' : '<td>',
 						parseFloat(data.value, 10).toFixed(3), ' ',
 						this._assignFlag(data.flag),
 					'</td>'
 				].join('');
-			} else {
-				td = [
-					'<td>',
-						parseFloat(data.value, 10).toFixed(3),
-					'</td>'
-				].join('');
-			}
 		} else {
 			td = '<td>--</td>';
 		}
