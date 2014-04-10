a7f2dddc45d07b3eb72612cd49f416b855518905
ehunter-usgs <ehunter@usgs.gov>
2014-04-10 13:19:24 -0600

    Remove pager/shakemap/dyfi paths.

--- test/index.js
+++ test/index.js
@@ -14,10 +14,7 @@ require.config({
 
 		base: 'base/0-0-1/js',
 		summary: 'summary/0-0-1/js',
-		dyfi: 'dyfi/0-0-1/js',
-		pager: 'pager/0-0-1/js',
 		scientific: 'scientific/0-0-1/js',
-		shakemap: 'shakemap/0-0-1/js',
 		impact: 'impact/0-0-1/js'
 	},
 	shim: {
