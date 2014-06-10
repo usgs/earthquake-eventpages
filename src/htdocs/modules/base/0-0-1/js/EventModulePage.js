838d79975f279fb2716b154af6d02f371ca51d4d
Hal Simpson <hasimpson@usgs.gov>
2014-06-10 16:11:22 -0600

    Added getDownloads, and _getProducts to centralize the download lists into the EventModulePage class,  and the footer section.

--- src/htdocs/modules/base/0-0-1/js/EventModulePage.js
+++ src/htdocs/modules/base/0-0-1/js/EventModulePage.js
@@ -2,11 +2,13 @@
 define([
 	'base/EventModule',
 
-	'util/Util'
+	'util/Util',
+	'base/ContentsXML'
 ], function (
 	EventModule,
 
-	Util
+	Util,
+	ContentsXML
 ) {
 	'use strict';
 
@@ -26,6 +28,7 @@ define([
 		this._hash = options.hash || DEFAULTS.hash;
 		this._title = options.title || DEFAULTS.title;
 		this._event = options.eventDetails || {};
+		this._productTypes = options.productTypes || [];
 
 		this._initialize();
 	};
@@ -83,13 +86,82 @@ define([
 	};
 
 	EventModulePage.prototype._setFooterMarkup = function () {
-		var footerMarkup = this._module.getFooterMarkup(this);
+		var footerMarkup = this._module.getFooterMarkup(this),
+		    products,
+		    el,
+		    i;
 
+		//This isn't currently used. But it makes sense to leave it.
 		if (typeof footerMarkup === 'object') {
 			this._footer.appendChild(footerMarkup);
 		} else {
 			this._footer.innerHTML = footerMarkup;
 		}
+
+		//Get a list of products to add to the downloads list.
+		products = this._getProducts();
+
+		//IRIS page, and tests can return no products.
+		if (products.length !== 0) {
+
+			el = document.createElement('div');
+			el.innerHTML = '<p><header><h3>Downloads</h3></header></p>';
+			el.className = 'downloads';
+			this._footer.appendChild(el);
+
+			for (i=0; i< products.length; i++) {
+				this.getDownloads(products[i]);
+			}
+		}
+
+	};
+
+	/**
+	 * Find the products to display on this page.
+	 *
+	 * @return {Array<Object>} allProducts,
+	 *         all products that match options.productTypes.
+	 *
+	 * Named '_getProducts' to not conflict with the tablist
+	 * call 'getProducts' that was overridden in MomentTensorPage.js
+	 */
+	EventModulePage.prototype._getProducts = function () {
+		var productTypes = this._productTypes,
+		    products = [],
+		    allProducts = [],
+		    type;
+
+		// loop through different productTypes
+		for (var i = 0; i < productTypes.length; i++) {
+			type = productTypes[i];
+			products = this._event.properties.products[type] || [];
+			allProducts = allProducts.concat(products);
+		}
+
+		return allProducts;
+	};
+
+	/**
+	 * Gets the downloadable products and attachs to the footer.
+	 */
+	EventModulePage.prototype.getDownloads = function (product) {
+		var el = document.createElement('div');
+
+		el.innerHTML = '<p>Loading contents &hellip;</p>';
+		el.className = 'downloads';
+
+			new ContentsXML({
+				product: product,
+				callback: function (contents) {
+					// build content
+					el.innerHTML = contents.getDownloads();
+				},
+				errback: function () {
+					el.innerHTML =
+							'<p class="alert error">Unable to load downloads &hellip;</p>';
+				}});
+
+			this._footer.appendChild(el);
 	};
 
 
