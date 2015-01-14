9c82706be0173a6c66f7a347d5e4df635d690f2b
ehunter-usgs <ehunter@usgs.gov>
2015-01-14 13:11:32 -0700

    Update link to be site relative.

--- src/htdocs/terms.php
+++ src/htdocs/terms.php
@@ -318,7 +318,6 @@ if (!isset($TEMPLATE)) {
 <p>
   A mathematical representation of the movement on a fault during an earthquake.
   The tensor depends on the source strength and fault orientation. See also
-  <a href="http://earthquake.usgs.gov/learn/topics/beachball.php">Focal
-  Mechanisms</a>
+  <a href="/learn/topics/beachball.php">Focal Mechanisms</a>
 </p>
 
