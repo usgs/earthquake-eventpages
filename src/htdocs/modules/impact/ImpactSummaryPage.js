7e73c77c7fca105b72c9854a484a5b4ea766a2a9
ehunter-usgs <ehunter@usgs.gov>
2015-03-12 14:11:56 -0600

    Add shakemap summaries to the impact summary page.

--- src/htdocs/modules/impact/ImpactSummaryPage.js
+++ src/htdocs/modules/impact/ImpactSummaryPage.js
@@ -34,8 +34,37 @@ ImpactSummaryPage.prototype._setFooterMarkup = function () {};
  * Render page content.
  */
 ImpactSummaryPage.prototype._setContentMarkup = function () {
-  this._content.innerHTML = '';
+  var products = this._options.eventDetails.properties.products,
+      product;
+
+  // Shakemap content
+  if (products.hasOwnProperty('shakemap')) {
+    product = products.shakemap[0];
+    this._getPreferredSummaryMarkup(product, 'impact_shakemap', 'ShakeMap');
+  }
 };
 
+ImpactSummaryPage.prototype._getPreferredSummaryMarkup = function (product, hash, name) {
+  var preferredProductMarkup = document.createElement('section');
+
+  this._options.module.getPage(hash, function (page) {
+    var products = page.getProducts(),
+        preferredLink = document.createElement('a');
+
+    preferredProductMarkup.innerHTML = '<h3>' + name + '</h3>';
+    preferredProductMarkup.appendChild(page.buildSummaryMarkup(product));
+
+    // Add link to product-summary page when more than one product exists
+    if (products.length > 1) {
+      preferredLink.href = '#' + hash;
+      preferredLink.className = 'view-all';
+      preferredLink.innerHTML = 'View all ' + name + 's (' + products.length +
+          ' total)';
+      preferredProductMarkup.appendChild(preferredLink);
+    }
+  });
+
+  this._content.appendChild(preferredProductMarkup);
+};
 
 module.exports = ImpactSummaryPage;
