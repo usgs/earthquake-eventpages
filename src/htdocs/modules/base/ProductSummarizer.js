b84113c24d842be02707d4fea3d2c197a47649dc
Eric Martinez <emartinez@usgs.gov>
2015-12-09 08:15:57 -0700

    Added documentation.

--- src/htdocs/modules/base/ProductSummarizer.js
+++ src/htdocs/modules/base/ProductSummarizer.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * This is a static utility class that knows how to summarize each notable
+ * product. The generalized `getProductSummary` method is the primary entry
+ * point for product summarization, however each notable product may be
+ * directly summarized as well.
+ *
+ * @see #getProductSummary
+ */
 
 var Attribution = require('./Attribution'),
     BeachBall = require('scientific/tensor/BeachBall'),
@@ -8,21 +16,121 @@ var Attribution = require('./Attribution'),
     Tensor = require('scientific/tensor/Tensor');
 
 
-var _getDYFISummary,
-  _getFiniteFaultSummary,
-  _getFocalMechanismSummary,
-  _getLossPAGERSummary,
-  _getMomentTensorSummary,
-  _getOriginProducts,
-  _getOriginSummary,
-  _getShakeMapSummary,
+var _formatter,
 
-  getProductSummary;
+    _getBeachBall,
+    _getOriginProducts,
 
-var _formatter = new Formatter();
+    getDYFISummary,
+    getFiniteFaultSummary,
+    getFocalMechanismSummary,
+    getLossPAGERSummary,
+    getMomentTensorSummary,
+    getOriginSummary,
+    getProductSummary,
+    getShakeMapSummary;
 
 
-_getDYFISummary = function (product) {
+/* Formatter used throughout the class */
+_formatter = new Formatter();
+
+
+/**
+ * @PrivateMethod
+ *
+ * Produces a data URL for a beach ball image the represents the given tensor.
+ *
+ * @param tensor {scientific/tensor/Tensor}
+ *
+ * @return {scientific/tensor/BeachBall}
+ */
+_getBeachBall = function (tensor) {
+  return new BeachBall({
+    tensor: tensor,
+    size: 200,
+    plotAxes: false,
+    plotPlanes: true,
+    fillColor: tensor.fillColor,
+  }).getCanvas().toDataURL();
+};
+
+/**
+ * Determines the "origin" products from a keyed object of all products for a
+ * given event. Considers phase-data and actual origin products as a potential
+ * "origin" product.
+ *
+ * If a unique (source+sourceCode) phase-data or origin product exists, it is
+ * considered an "origin" product for the event.
+ *
+ * If phase-data and origin product exist with the same source+sourceCode, the
+ * most recently updated of the two is considered an "origin" product for the
+ * event and the other is discarded. If thw two products have the same
+ * update time, the phase-data product is considered an "origin" product and the
+ * origin itself is discarded.
+ *
+ * @param products {Object}
+ *      An object keyed by product type whose corresponding values are an
+ *      array of products of that type in descending preferrred-ness order.
+ *
+ * @return {Array}
+ *      An array of origin-like products in descending preferred order.
+ */
+_getOriginProducts = function (products) {
+  var index,
+      originProducts,
+      originSourceCodes,
+      phaseProducts,
+      phaseSourceCode;
+
+  originSourceCodes = [];
+
+  try {
+    originProducts = products.origin.slice(0);
+    phaseProducts = products['phase-data'];
+
+    if (!phaseProducts) {
+      return originProducts;
+    }
+
+    // Build list of origin ids for lookup later
+    originSourceCodes = originProducts.map(function (origin) {
+      return origin.source + '_' + origin.code;
+    });
+
+    // Look at phase data products, if an older corresponding origin exists,
+    // replace it with the phase data product instead, otherwise append phase
+    // data product to list of origins
+    phaseProducts.forEach(function (phaseData) {
+      phaseSourceCode = phaseData.source + '_' + phaseData.code;
+      index = originSourceCodes.indexOf(phaseSourceCode);
+
+      if (index === -1) {
+        originProducts.push(phaseData);
+        originSourceCodes.push(phaseSourceCode);
+      } else if (originProducts[index].updateTime <= phaseData.updateTime) {
+        originProducts[index] = phaseData;
+      }
+    });
+  } catch (e) {
+    originProducts = [];
+  } finally {
+    return originProducts;
+  }
+};
+
+
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "dyfi" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a DYFI product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getDYFISummary = function (product) {
   var properties = product.properties,
       contents = product.contents,
       eventId = properties.eventsource + properties.eventsourcecode,
@@ -47,7 +155,18 @@ _getDYFISummary = function (product) {
     '</ul>';
 };
 
-_getFiniteFaultSummary = function (product) {
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "finite-fault" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a Finite Fault product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getFiniteFaultSummary = function (product) {
   var basemap;
 
   basemap = product.contents['basemap.png'];
@@ -63,7 +182,18 @@ _getFiniteFaultSummary = function (product) {
     '</ul>';
 };
 
-_getFocalMechanismSummary = function (product) {
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "focal-mechanism" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a Focal Mechanism product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getFocalMechanismSummary = function (product) {
   var beachBall,
       np1,
       np2,
@@ -80,13 +210,7 @@ _getFocalMechanismSummary = function (product) {
     source = product.source;
   }
 
-  beachBall = new BeachBall({
-    tensor: tensor,
-    size: 200,
-    plotAxes: false,
-    plotPlanes: true,
-    fillColor: tensor.fillColor,
-  }).getCanvas().toDataURL();
+  beachBall = _getBeachBall(tensor);
 
   return [
         '<ul>',
@@ -131,7 +255,18 @@ _getFocalMechanismSummary = function (product) {
   ].join('');
 };
 
-_getLossPAGERSummary = function (product) {
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "losspager" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a LossPAGER product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getLossPAGERSummary = function (product) {
   var alertlevel,
       properties;
 
@@ -149,7 +284,18 @@ _getLossPAGERSummary = function (product) {
     '</ul>';
 };
 
-_getMomentTensorSummary = function (product) {  var beachBall,
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "moment-tensor" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a Moment Tensor product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getMomentTensorSummary = function (product) {  var beachBall,
       code,
       depth,
       magnitude,
@@ -217,67 +363,17 @@ _getMomentTensorSummary = function (product) {  var beachBall,
 };
 
 /**
- * Determines the product to use as the "origin" product for this event.
+ * @APIMethod
  *
- * If both phase-data and origin product exist, use the most recently updated
- * product as the "origin" product.
+ * Gets the summary markup for a given "origin" product.
  *
- * If only one of phase-data or origin product exist, use it as the "origin"
- * product.
+ * @param product {Object}
+ *      An object with properties commonly found on a Origin product.
  *
- * If neither product exists (unlikely), then assume no "origin" product.
- *
- * @param products {Object}
- *      An object keyed by product type whose corresponding values are an
- *      array of products of that type in descending preferrred-ness order.
- *
- * @return {Array}
- *      An array of origin-like products in descending preferred order.
+ * @return {String}
+ *      Markup that summarizes the product.
  */
-_getOriginProducts = function (products) {
-  var index,
-      originProducts,
-      originSourceCodes,
-      phaseProducts,
-      phaseSourceCode;
-
-  originSourceCodes = [];
-
-  try {
-    originProducts = products.origin.slice(0);
-    phaseProducts = products['phase-data'];
-
-    if (!phaseProducts) {
-      return originProducts;
-    }
-
-    // Build list of origin ids for lookup later
-    originSourceCodes = originProducts.map(function (origin) {
-      return origin.source + '_' + origin.code;
-    });
-
-    // Look at phase data products, if an older corresponding origin exists,
-    // replace it with the phase data product instead, otherwise append phase
-    // data product to list of origins
-    phaseProducts.forEach(function (phaseData) {
-      phaseSourceCode = phaseData.source + '_' + phaseData.code;
-      index = originSourceCodes.indexOf(phaseSourceCode);
-
-      if (index === -1) {
-        originProducts.push(phaseData);
-        originSourceCodes.push(phaseSourceCode);
-      } else if (originProducts[index].updateTime <= phaseData.updateTime) {
-        originProducts[index] = phaseData;
-      }
-    });
-  } catch (e) {
-    originProducts = [];
-  } finally {
-    return originProducts;
-  }
-};
-
-_getOriginSummary = function (product) {
+getOriginSummary = function (product) {
   var p,
       depth,
       magnitude,
@@ -325,7 +421,73 @@ _getOriginSummary = function (product) {
       '</ul>';
 };
 
-_getShakeMapSummary = function (product) {
+/**
+ * @APIMethod
+ *
+ * @param type {String}
+ *      Product type
+ * @param hash {String}
+ *      Link to product details
+ * @param name {String}
+ *      Header text used to label the product summary
+ * @param eventDetails {Object}
+ *      Object with properties representing the current event
+ *
+ * @return {String}
+ *      Markup for the product summary
+ */
+getProductSummary = function (type, hash, name, eventDetails) {
+  var allProducts,
+      products,
+      summary;
+
+  allProducts = eventDetails.properties.products;
+  summary = [];
+  products = allProducts[type];
+
+  if (type === 'origin' || type === 'phase-data') {
+    products = getOriginProducts(allProducts);
+    summary = getOriginSummary(products[0]);
+  } else if (type === 'dyfi') {
+    summary = getDYFISummary(products[0]);
+  } else if (type === 'shakemap') {
+    summary = getShakeMapSummary(products[0]);
+  } else if (type === 'losspager') {
+    summary = getLossPAGERSummary(products[0]);
+  } else if (type === 'moment-tensor') {
+    summary = getMomentTensorSummary(products[0]);
+  } else if (type === 'focal-mechanism') {
+    summary = getFocalMechanismSummary(products[0]);
+  } else if (type === 'finite-fault') {
+    summary = getFiniteFaultSummary(products[0]);
+  }
+
+  if (summary) {
+    return [
+      '<h3>', name, '</h3>',
+      '<a href="#', hash, '" class="horizontal-scrolling ',
+          type, '-summary summary">',
+        summary,
+      '</a>'
+    ].join('');
+  } else {
+    return '<p class="alert warning">' +
+        'No product summary for product type &ldquo;' + type + '&rdquo;</p>';
+  }
+};
+
+/**
+ * @APIMethod
+ *
+ * Gets the summary markup for a given "shakemap" product.
+ *
+ * @param product {Object}
+ *      An object with properties commonly found on a ShakeMap product.
+ *
+ * @return {String}
+ *      Markup that summarizes the product.
+ */
+getShakeMapSummary = function (product) {
   var properties = product.properties,
       contents = product.contents,
       maxmmi = properties.maxmmi,
@@ -357,55 +519,14 @@ _getShakeMapSummary = function (product) {
 };
 
 
-getProductSummary = function (type, hash, name, eventDetails) {
-  var allProducts,
-      products,
-      summary;
-
-  allProducts = eventDetails.properties.products;
-  summary = [];
-  products = allProducts[type];
-
-  if (type === 'origin' || type === 'phase-data') {
-    products = _getOriginProducts(allProducts);
-    summary = _getOriginSummary(products[0]);
-  } else if (type === 'dyfi') {
-    summary = _getDYFISummary(products[0]);
-  } else if (type === 'shakemap') {
-    summary = _getShakeMapSummary(products[0]);
-  } else if (type === 'losspager') {
-    summary = _getLossPAGERSummary(products[0]);
-  } else if (type === 'moment-tensor') {
-    summary = _getMomentTensorSummary(products[0]);
-  } else if (type === 'focal-mechanism') {
-    summary = _getFocalMechanismSummary(products[0]);
-  } else if (type === 'finite-fault') {
-    summary = _getFiniteFaultSummary(products[0]);
-  }
-
-  if (summary) {
-    return [
-      '<h3>', name, '</h3>',
-      '<a href="#', hash, '" class="horizontal-scrolling ',
-          type, '-summary summary">',
-        summary,
-      '</a>'
-    ].join('');
-  } else {
-    return '<p class="alert warning">' +
-        'No product summary for product type &ldquo;' + type + '&rdquo;</p>';
-  }
-};
-
-
+// Export the API
 module.exports = {
+  getDYFISummary: getDYFISummary,
+  getFiniteFaultSummary: getFiniteFaultSummary,
+  getFocalMechanismSummary: getFocalMechanismSummary,
+  getLossPAGERSummary: getLossPAGERSummary,
+  getMomentTensorSummary: getMomentTensorSummary,
+  getOriginSummary: getOriginSummary,
   getProductSummary: getProductSummary,
-
-  getDYFISummary: _getDYFISummary,
-  getFiniteFaultSummary: _getFiniteFaultSummary,
-  getFocalMechanismSummary: _getFocalMechanismSummary,
-  getLossPAGERSummary: _getLossPAGERSummary,
-  getMomentTensorSummary: _getMomentTensorSummary,
-  getOriginSummary: _getOriginSummary,
-  getShakeMapSummary: _getShakeMapSummary
+  getShakeMapSummary: getShakeMapSummary
 };
