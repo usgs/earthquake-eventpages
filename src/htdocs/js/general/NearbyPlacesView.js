c8586fb9bfcb151208273960dcbb97b19a505702
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-03-08 14:47:50 -0700

    created/setup nearby places view

--- src/htdocs/js/general/NearbyPlacesView.js
+++ src/htdocs/js/general/NearbyPlacesView.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var ProductView = require('core/ProductView');
+var ContentView = require('core/ContentView'),
+    Formatter = require('core/Formatter'),
+    Util = require('util/Util');
 
 var NearbyPlacesView = function (options) {
   var _this,
@@ -8,15 +10,48 @@ var NearbyPlacesView = function (options) {
 
       _formatter;
 
-  _this = ProductView(options);
+  options = options || {};
+  _this = ContentView(options);
 
   _initialize = function (options) {
     _formatter = options.formatter || Formatter();
   };
 
-  _this.render = function () {
-    _this.el.className = 
+  _this.onSuccess = function (data) {
+    var i,
+        len,
+        markup;
+
+    len = data.length;
+    markup = [];
+
+    _this.el.className = 'nearby-places';
+    markup.push('<h3>Nearby Places</h3>');
+    // Formats nearby places
+    for (i = 0; i < len; i++) {
+      markup.push('<li>' +
+          data[i].distance + ' km ' +
+          '(' + Math.round(_formatter.kmToMi(data[i].distance)) + ' mi) ' +
+          data[i].direction +
+          ' of ' + data[i].name +
+          '</li>');
+    }
+
+    _this.el.innerHTML = '<ul class="no-style">' + markup.join('') + '</ul>';
   };
+
+  /**
+   * Free references.
+   */
+  _this.destroy = Util.compose(function () {
+    _formatter = null;
+    _this = null;
+    _initialize = null;
+  }, _this.destroy);
+
+  _initialize(options);
+  options = null;
+  return _this;
 };
 
 module.exports = NearbyPlacesView;
