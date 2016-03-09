bcd5ff6798fe3e4a64710bbf1e2a3bd4033f7750
ehunter-usgs <ehunter@usgs.gov>
2016-03-09 11:30:21 -0700

    Update example page with correct css file so that tablist is styled

--- example/shakemap/ShakeMapViewExample.php
+++ example/shakemap/ShakeMapViewExample.php
@@ -9,7 +9,7 @@ if (!isset($TEMPLATE)) {
 
   // Stuff that goes at the top of the page (in the <head>) (i.e. <link> tags)
   $HEAD = '
-    <link rel="stylesheet" href="/css/event.css"/>
+    <link rel="stylesheet" href="/css/index.css"/>
   ';
 
   // Stuff that goes at the bottom of the page (i.e. <script> tags)
