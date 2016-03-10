f9f678b6743d68818e734af5a83ea7b92f9e2ea8
Hal Simpson <hasimpson@usgs.gov>
2016-03-10 10:52:29 -0700

    Fixed my misunderstanding from the merge.   dot not comma

--- example/_navigation.inc.php
+++ example/_navigation.inc.php
@@ -10,7 +10,7 @@ print navGroup('General',
 
 print navGroup('Impact',
     navItem('/shakemap/ShakeMapStationListViewExample.php',
-        'ShakeMapStationListView'),
+        'ShakeMapStationListView ') .
     navItem('/dyfi/DYFIResponsesViewExample.php', 'DYFIResponsesView')
   );
 ?>
