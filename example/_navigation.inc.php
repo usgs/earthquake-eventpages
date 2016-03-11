61c1e96128228335b0d37e30b48b9874440427ef
ehunter-usgs <ehunter@usgs.gov>
2016-03-11 14:27:27 -0700

    Add ShakeMapView example to Impact section

--- example/_navigation.inc.php
+++ example/_navigation.inc.php
@@ -10,13 +10,10 @@ print navGroup('General',
   );
 
 print navGroup('Impact',
+    navItem('/dyfi/DYFIResponsesViewExample.php', 'DYFIResponsesView') .
     navItem('/shakemap/ShakeMapInfoViewExample.php', 'ShakeMapInfoView') .
     navItem('/shakemap/ShakeMapStationListViewExample.php',
         'ShakeMapStationListView ') .
-    navItem('/dyfi/DYFIResponsesViewExample.php', 'DYFIResponsesView')
-  );
-
-print navGroup('ShakeMap',
     navItem('/shakemap/ShakeMapViewExample.php', 'ShakeMapView')
   );
 
