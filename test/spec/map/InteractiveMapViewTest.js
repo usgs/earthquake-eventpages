38924b1427405e02335e4935fd4ef37587af5fbc
Eric Martinez <emartinez@usgs.gov>
2016-03-16 12:51:28 -0600

    Finished tests for addDfyiOverlays method.

--- test/spec/map/InteractiveMapViewTest.js
+++ test/spec/map/InteractiveMapViewTest.js
@@ -70,11 +70,76 @@ describe('map/InteractiveMapView', function () {
     });
 
     it('does not include default if either 10k or 1k is present', function () {
+      var dyfi,
+          spy,
+          view;
 
+      view = InteractiveMapView();
+
+      // 10km is present
+      dyfi = Product({
+        contents: {
+          'dyfi_geo_10km.geojson': {'url': '10k'},
+          'dyfi_geo.geojson': {'url': 'default'}
+        }
+      });
+
+      spy = sinon.spy(view, 'createDyfiUtmLayer');
+      view.addDyfiOverlays(dyfi);
+
+      expect(spy.callCount).to.equal(1);
+      /* jshint -W030 */
+      expect(spy.calledWith('default')).to.be.false;
+      /* jshint -W030 */
+
+      // 1k is present
+      spy.restore();
+      dyfi.destroy();
+
+      dyfi = Product({
+        contents: {
+          'dyfi_geo_1km.geojson': {'url': '1k'},
+          'dyfi_geo.geojson': {'url': 'default'}
+        }
+      });
+
+      spy = sinon.spy(view, 'createDyfiUtmLayer');
+      view.addDyfiOverlays(dyfi);
+
+      expect(spy.callCount).to.equal(1);
+      /* jshint -W030 */
+      expect(spy.calledWith('default')).to.be.false;
+      /* jshint -W030 */
+
+      spy.restore();
+      dyfi.destroy();
     });
 
     it('includes default if neither 10k or 1k is present', function () {
+      var dyfi,
+          spy,
+          view;
 
+      view = InteractiveMapView();
+
+      dyfi = Product({
+        contents: {
+          'dyfi_geo.geojson': {'url': 'default'}
+        }
+      });
+
+      spy = sinon.spy(view, 'createDyfiUtmLayer');
+      view.addDyfiOverlays(dyfi);
+
+      expect(spy.callCount).to.equal(1);
+      /* jshint -W030 */
+      expect(spy.calledWith('10k')).to.be.false;
+      expect(spy.calledWith('1k')).to.be.false;
+      expect(spy.calledWith('default')).to.be.true;
+      /* jshint -W030 */
+
+      spy.restore();
+      dyfi.destroy();
     });
   });
 });
