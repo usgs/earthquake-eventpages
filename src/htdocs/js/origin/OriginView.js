9f295bacf5e2cda383925d63f748413cf690bcb4
ehunter-usgs <ehunter@usgs.gov>
2016-03-22 14:27:38 -0600

    Add Origin detail table to OriginView

--- src/htdocs/js/origin/OriginView.js
+++ src/htdocs/js/origin/OriginView.js
@@ -1,20 +1,170 @@
 'use strict';
 
-var ProductView = require('core/ProductView');
+var Attribution = require('core/Attribution'),
+    Formatter = require('core/Formatter'),
+    ProductView = require('core/ProductView'),
+    Util = require('util/Util');
+
+var _DEFAULTS = {
+
+};
+var NOT_REPORTED = '&ndash;';
 
 var OriginView = function (options) {
 
-  var _this;
+  var _this,
+      _initialize,
 
+      _formatter;
+
+  options = Util.extend({}, _DEFAULTS, options);
   _this = ProductView(options);
 
-  _this.render = function () {
-    _this.el.innerHTML = '<p>TODO :: port HypocenterView to OriginView</p>';
+  _initialize = function (options) {
+    _formatter = options.formatter || Formatter();
   };
 
+  _this.getOriginDetailTable = function (product) {
+    var azimuthalGap,
+        buf,
+        depth,
+        depthError,
+        eventTime,
+        horizontalError,
+        latitude,
+        longitude,
+        magnitude,
+        magnitudeError,
+        magnitudeSource,
+        magnitudeType,
+        minimumDistance,
+        numPhases,
+        numStations,
+        originSource,
+        p,
+        reviewStatus,
+        standardError;
+
+    buf = [];
+    p = product.properties;
+
+    // required attributes for origins
+    latitude = p.latitude;
+    longitude = p.longitude;
+    eventTime = p.eventtime;
+
+    // optional attributes for origins
+    magnitude = p.magnitude || null;
+    magnitudeType = p['magnitude-type'] || null;
+    magnitudeError = p['magnitude-error'] || null;
+    horizontalError = p['horizontal-error'] || null;
+    depth = p.depth || null;
+    depthError = p['vertical-error'] || null;
+    numStations = p['num-stations-used'] || null;
+    numPhases = p['num-phases-used'] || null;
+    minimumDistance = p['minimum-distance'] || null;
+    standardError = p['standard-error'] || null;
+    azimuthalGap = p['azimuthal-gap'] || null;
+    reviewStatus = p['review-status'] || 'automatic';
+    originSource = p['origin-source'] || product.source;
+    magnitudeSource = p['magnitude-source'] || product.source;
+
+
+    buf.push(
+      '<div class="horizontal-scrolling">',
+      '<table class="origin-detail"><tbody>'
+    );
+
+    buf.push('<tr><th scope="row">Magnitude',
+        (magnitudeError ? '<span class="uncertainty">uncertainty</span>' : ''),
+        '</th><td>',
+        _formatter.magnitude(magnitude, magnitudeType, magnitudeError),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Location',
+        (horizontalError ? '<span class="uncertainty">uncertainty</span>' : ''),
+        '</th><td>',
+        _formatter.location(latitude, longitude),
+        _formatter.uncertainty(horizontalError, 1, NOT_REPORTED, 'km'),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Depth',
+        (depthError ? '<span class="uncertainty">uncertainty</span>' : ''),
+        '</th><td>',
+        _formatter.depth(depth, 'km', depthError) +
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Origin Time</th><td>',
+        (typeof eventTime === 'string' ?
+            '<time datetime="' + eventTime + '">' +
+                eventTime.replace('T', ' ').replace('Z', ' UTC') +
+            '</time>' :
+            ''),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Number of Stations</th><td>',
+        (numStations === null ? NOT_REPORTED : numStations),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Number of Phases</th><td>',
+        (numPhases === null ? NOT_REPORTED : numPhases),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Minimum Distance</th><td>',
+        (minimumDistance === null ? NOT_REPORTED :
+            (minimumDistance * 0.0174532925 * 6378.1).toFixed(2) + ' km' +
+            ' (' + parseFloat(minimumDistance).toFixed(2) + '&deg;)'),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Travel Time Residual</th><td>',
+        (standardError === null ? NOT_REPORTED : standardError + ' sec'),
+        '</td></tr>');
+
+    buf.push('<tr><th scope="row">Azimuthal Gap</th><td>',
+        (azimuthalGap === null ? NOT_REPORTED : azimuthalGap + '&deg;'),
+        '</td></tr>');
+
+    // Placeholder, filled in asynchronously
+    buf.push('<tr>',
+        '<th scope="row">',
+          '<abbr title="Flinn Engdahl">FE</abbr> Region',
+        '</th>',
+        '<td class="fe-info">' + NOT_REPORTED + '</td></tr>');
+
+    buf.push('<tr><th scope="row">Review Status</th><td>',
+        reviewStatus.toUpperCase().replace('REVIEWED', 'MANUAL'),
+        '</td></tr>');
+
+    buf.push(
+        '<tr><th scope="row">Catalog</th><td>',
+          _formatter.catalogDetail(product),
+        '</td></tr>',
+        '<tr><th scope="row">Location Source</th><td>',
+          Attribution.getContributorReference(originSource),
+        '</td></tr>',
+        '<tr><th scope="row">Magnitude Source</th><td>',
+          Attribution.getContributorReference(magnitudeSource),
+        '</td></tr>',
+        '<tr><th scope="row">Contributor</th><td>',
+          Attribution.getContributorReference(product.source),
+        '</td></tr>');
+
+    buf.push('</tbody></table></div>');
+
+    return buf.join('');
+  };
+
+  _this.render = function () {
+    _this.el.innerHTML = _this.getOriginDetailTable(_this.model.get());
+  };
+
+
+  _initialize(options);
   options = null;
   return _this;
 };
+
+
 
 
 module.exports = OriginView;
