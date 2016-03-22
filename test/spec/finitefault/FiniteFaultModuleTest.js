6bcd0399f2263339a30b6bd8c66e7aff386ba072
ehunter-usgs <ehunter@usgs.gov>
2016-03-22 09:30:16 -0600

    Update tests for FiniteFaultModule

--- test/spec/finitefault/FiniteFaultModuleTest.js
+++ test/spec/finitefault/FiniteFaultModuleTest.js
@@ -1,17 +1,84 @@
-/* global chai, describe, it */
+/* global before, beforeEach, chai, describe, it, sinon */
 'use strict';
 
-var FiniteFaultModule = require('finitefault/FiniteFaultModule');
+var CatalogEvent = require('pdl/CatalogEvent'),
+    FiniteFaultModule = require('finitefault/FiniteFaultModule'),
+    Model = require('mvc/Model'),
+    Xhr = require('util/Xhr');
 
 
 var expect = chai.expect;
 
 
 describe('finitefault/FiniteFaultModule', function () {
+  var eventModel;
+
+  before(function (done) {
+    Xhr.ajax({
+      url: '/events/us10004u1y.json',
+      success: function (details) {
+        eventModel = Model({
+          'event': CatalogEvent(details),
+          'config': {}
+        });
+        done();
+      },
+      error: function () {
+        done();
+      }
+    });
+  });
 
   describe('constructor', function () {
     it('should be defined', function () {
       expect(typeof FiniteFaultModule).to.equal('function');
+    });
+  });
+
+  describe('render', function () {
+    var module;
+
+    beforeEach(function () {
+      module = FiniteFaultModule({
+        el: document.createElement('div'),
+        model: eventModel
+      });
+    });
+
+    it('adds the header title', function () {
+      var header;
+      module.render();
+
+      header = module.header.querySelector('h3');
+      /* jshint -W030 */
+      expect(header).to.not.be.null;
+      /* jshint +W030 */
+      expect(header.innerHTML).to.equal('Finite Fault');
+    });
+
+    it('includes the default module header', function () {
+      var spy;
+
+      spy = sinon.spy(module, 'getProductHeader');
+      module.render();
+
+      expect(spy.callCount).to.equal(1);
+      spy.restore();
+    });
+
+    it('includes no footer', function () {
+      var content,
+          footer;
+
+      module.render();
+
+      content = module.content;
+      footer = module.footer;
+      /* jshint -W030 */
+      expect(footer).to.not.be.null;
+      /* jshint +W030 */
+      expect(module.el.lastElementChild).to.equal(content);
+      expect(module.el.lastElementChild).to.not.equal(footer);
     });
   });
 
