7dc43c3645b0fd0359e2ecfb8cb316ed686c6942
Eric Martinez <emartinez@usgs.gov>
2016-03-24 07:46:39 -0600

    Added "it" calls to actually run the tests.

--- test/spec/shakemap/ShakeMapInfoViewTest.js
+++ test/spec/shakemap/ShakeMapInfoViewTest.js
@@ -8,7 +8,6 @@ var expect = chai.expect;
 
 
 describe('shakemap/ShakeMapInfoView', function () {
-
   describe('constructor', function () {
     it('is a function', function () {
       expect(typeof ShakeMapInfoView).to.equal('function');
@@ -16,59 +15,62 @@ describe('shakemap/ShakeMapInfoView', function () {
   });
 
   describe('formatOutputGroundMotions', function () {
-    var el,
-        view;
+    it('should have some headers', function () {
+      var el,
+          view;
 
-    view = ShakeMapInfoView();
-    el = document.createElement('div');
-    el.innerHTML = view.formatOutputGroundMotions({
-      'unknown': {
-        bias: 4,
-        max: 5,
-        max_grid: 6,
-        units: 'y'
-      },
-      'intensity': {
-        bias: 1,
-        max: 2,
-        max_grid: 3,
-        units: 'x'
-      }
+      view = ShakeMapInfoView();
+      el = document.createElement('div');
+      el.innerHTML = view.formatOutputGroundMotions({
+        'unknown': {
+          bias: 4,
+          max: 5,
+          max_grid: 6,
+          units: 'y'
+        },
+        'intensity': {
+          bias: 1,
+          max: 2,
+          max_grid: 3,
+          units: 'x'
+        }
+      });
+
+      // uses expected heading for known ground motions,
+      // and places known fields first
+      expect(el.querySelector('tbody > tr > th').innerHTML)
+          .to.equal('Intensity');
+      // displays unknown ground motions, after known
+      expect(el.querySelector('tbody > tr + tr > th').innerHTML)
+          .to.equal('unknown');
     });
-
-    // uses expected heading for known ground motions,
-    // and places known fields first
-    expect(el.querySelector('tbody > tr > th').innerHTML).to.equal('Intensity');
-    // displays unknown ground motions, after known
-    expect(el.querySelector('tbody > tr + tr > th').innerHTML).to.equal(
-        'unknown');
   });
 
   describe('formatProcessingGroundMotions', function () {
-    var el,
-        view;
+    it('should have some headers', function () {
+      var el,
+          view;
 
-    view = ShakeMapInfoView();
-    el = document.createElement('div');
-    el.innerHTML = view.formatProcessingGroundMotions({
-      'unknown': {
-        module: 'x',
-        reference: 'y'
-      },
-      'directivity': {
-        module: 'u',
-        reference: 'v'
-      }
+      view = ShakeMapInfoView();
+      el = document.createElement('div');
+      el.innerHTML = view.formatProcessingGroundMotions({
+        'unknown': {
+          module: 'x',
+          reference: 'y'
+        },
+        'directivity': {
+          module: 'u',
+          reference: 'v'
+        }
+      });
+
+      // uses expected heading for known ground motions,
+      // and places known fields first
+      expect(el.querySelector('tbody > tr > th').innerHTML)
+          .to.equal('Directivity');
+      // displays unknown ground motions, after known
+      expect(el.querySelector('tbody > tr + tr > th').innerHTML)
+          .to.equal('unknown');
     });
-
-    // uses expected heading for known ground motions,
-    // and places known fields first
-    expect(el.querySelector('tbody > tr > th').innerHTML).to.equal(
-        'Directivity');
-    // displays unknown ground motions, after known
-    expect(el.querySelector('tbody > tr + tr > th').innerHTML).to.equal(
-        'unknown');
-
   });
-
 });
