5843c838ebc4e0023eecca199f96b9e8ddd8fb2b
Eric Martinez <emartinez@usgs.gov>
2016-03-28 12:40:44 -0600

    Added test for createRow method.

--- test/spec/scientific/ScientificSummaryModuleTest.js
+++ test/spec/scientific/ScientificSummaryModuleTest.js
@@ -1,4 +1,4 @@
-/* global chai, describe, it */
+/* global afterEach, beforeEach, chai, describe, it */
 'use strict';
 
 
@@ -16,6 +16,42 @@ describe('scientific/ScientificSummaryModule', function () {
 
     it('can be instantiated', function () {
       expect(ScientificSummaryModule).to.not.throw(Error);
+    });
+  });
+
+  describe('createRow', function () {
+    var module;
+
+    afterEach(function () {
+      module.destroy();
+    });
+
+    beforeEach(function () {
+      module = ScientificSummaryModule();
+    });
+
+    it('returns a row', function () {
+      var row;
+
+      row = module.createRow();
+
+      expect(row.nodeName).to.equal('TR');
+    });
+
+    it('includes the preferred class if specified', function () {
+      var row;
+
+      row = module.createRow(true);
+
+      expect(row.classList.contains('preferred')).to.equal(true);
+    });
+
+    it('skips the preferred class if not specified', function () {
+      var row;
+
+      row = module.createRow(false);
+
+      expect(row.classList.contains('preferred')).to.equal(false);
     });
   });
 });
