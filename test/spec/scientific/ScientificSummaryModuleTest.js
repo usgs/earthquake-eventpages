f37e6d4762da4756f889680a4365eefa8598bba2
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-03-31 14:02:05 -0600

    added test

--- test/spec/scientific/ScientificSummaryModuleTest.js
+++ test/spec/scientific/ScientificSummaryModuleTest.js
@@ -9,6 +9,7 @@ var expect = chai.expect;
 
 
 describe('scientific/ScientificSummaryModule', function () {
+
   describe('constructor', function () {
     it('is defined', function () {
       expect(typeof ScientificSummaryModule).to.equal('function');
@@ -32,6 +33,94 @@ describe('scientific/ScientificSummaryModule', function () {
       expect(view.createSummary.callCount).to.equal(1);
 
       view.destroy();
+    });
+  });
+
+  describe('getFiniteFaultRow', function () {
+    it('Creates a row with summary information', function () {
+
+    });
+  });
+
+  describe('getFocalMechanismSummary', function () {
+    it('calls createSummary', function () {
+      var view;
+
+      view = ScientificSummaryModule();
+
+      sinon.stub(view, 'createSummary', function () {});
+
+      view.getFocalMechanismSummary();
+
+      expect(view.createSummary.callCount).to.equal(1);
+
+      view.destroy();
+    });
+  });
+
+  describe('getFocalMechanismRow', function () {
+    it('', function () {
+
+    });
+  });
+
+  describe('getLinksHeader', function () {
+    it('returns header', function () {
+      var view;
+
+      view = ScientificSummaryModule();
+
+      expect(view.getLinksHeader()).to.equal('<h3>Scientific and Technical Links</h3>');
+    });
+  });
+
+  describe('getMomentTensorSummary', function () {
+    it('calls createSummary', function () {
+      var view;
+
+      view = ScientificSummaryModule();
+
+      sinon.stub(view, 'createSummary', function () {});
+
+      view.getMomentTensorSummary();
+
+      expect(view.createSummary.callCount).to.equal(1);
+
+      view.destroy();
+    });
+  });
+
+  describe('getMomentTensorRow', function () {
+    it('', function () {
+
+    });
+  });
+
+  describe('getOriginProducts', function () {
+    it('', function () {
+
+    });
+  });
+
+  describe('getOriginSummary', function () {
+    it('calls createSummary', function () {
+      var view;
+
+      view = ScientificSummaryModule();
+
+      sinon.stub(view, 'createSummary', function () {});
+
+      view.getOriginSummary();
+
+      expect(view.createSummary.callCount).to.equal(1);
+
+      view.destroy();
+    });
+  });
+
+  describe('getOriginRow', function () {
+    it('', function () {
+
     });
   });
 
