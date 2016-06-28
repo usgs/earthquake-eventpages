570c85d5ebfbfbba689052df4540fcc2cf936eed
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-06-28 10:08:52 -0600

    created origin table for pin

--- src/htdocs/js/origin/OriginPinView.js
+++ src/htdocs/js/origin/OriginPinView.js
@@ -1,25 +1,80 @@
 'use strict';
 
 var BasicPinView = require('core/BasicPinView'),
-    Util = require('util/Util'),
-    View = require('mvc/View');
+    Formatter = require('core/Formatter'),
+    Util = require('util/Util');
 
-var _DEFAULTS = {
-
-};
 
 var OriginPinView = function (options) {
   var _this;
 
-  options = Util.extend({}, _DEFAULTS, options);
-  _this = View(options);
-
-  _this.destroy = Util.compose(function () {
-    _this = null;
-  }, _this.destroy);
+  options = Util.extend({}, options);
+  _this = BasicPinView(options);
 
   _this.renderPinContent = function () {
+    var depth,
+        depthError,
+        originTime,
+        latitude,
+        longitude,
+        magnitude,
+        magnitudeError,
+        magnitudeType,
+        product;
 
+    product = _this.model;
+
+    depth = product.getProperty('depth');
+    depthError = product.getProperty('vertical-error');
+    latitude = product.getProperty('latitude');
+    longitude = product.getProperty('longitude');
+    magnitude = product.getProperty('magnitude');
+    magnitudeError = product.getProperty('magnitude-error');
+    magnitudeType = product.getProperty('magnitude-type');
+    originTime = product.getProperty('eventtime');
+
+    _this.formatter = Formatter();
+
+    _this.content.innerHTML =
+      '<div>' +
+        '<table class="origin-table">' +
+          '<tbody>' +
+
+            '<tr>' +
+              '<th scope="row">Magnitude</th>' +
+              '<td>' +
+                _this.formatter.magnitude(
+                    magnitude,
+                    magnitudeType,
+                    magnitudeError
+                ) +
+              '</td>' +
+            '</tr>' +
+
+            '<tr>' +
+              '<th scope="row">Magnitude</th>' +
+              '<td>' +
+                _this.formatter.location(latitude, longitude) +
+              '</td>' +
+            '</tr>' +
+
+            '<tr>' +
+              '<th scope="row">Magnitude</th>' +
+              '<td>' +
+                _this.formatter.depth(depth, 'km', depthError) +
+              '</td>' +
+            '</tr>' +
+
+            '<tr>' +
+              '<th scope="row">Magnitude</th>' +
+              '<td>' +
+                _this.formatter.depth(depth, 'km', depthError) +
+              '</td>' +
+            '</tr>' +
+
+          '</tbody>' +
+        '</table>' +
+      '</div>';
   };
 
   options = null;
