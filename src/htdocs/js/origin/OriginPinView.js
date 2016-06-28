3914adbf4f41abd80e057627a08348e0a8ac356b
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-06-28 16:03:44 -0600

    switched from a table to a badge that has the magnitude, magnitude type, and review status

--- src/htdocs/js/origin/OriginPinView.js
+++ src/htdocs/js/origin/OriginPinView.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var BasicPinView = require('core/BasicPinView'),
-    Formatter = require('core/Formatter'),
     Util = require('util/Util');
 
 
@@ -12,68 +11,37 @@ var OriginPinView = function (options) {
   _this = BasicPinView(options);
 
   _this.renderPinContent = function () {
-    var depth,
-        depthError,
-        originTime,
-        latitude,
-        longitude,
-        magnitude,
-        magnitudeError,
+    var magnitude,
         magnitudeType,
-        product;
+        product,
+        reviewStatus;
 
     product = _this.model;
 
-    depth = product.getProperty('depth');
-    depthError = product.getProperty('vertical-error');
-    latitude = product.getProperty('latitude');
-    longitude = product.getProperty('longitude');
     magnitude = product.getProperty('magnitude');
-    magnitudeError = product.getProperty('magnitude-error');
     magnitudeType = product.getProperty('magnitude-type');
-    originTime = product.getProperty('eventtime');
-
-    _this.formatter = Formatter();
+    reviewStatus = product.getProperty('review-status') || 'automatic';
 
     _this.content.innerHTML =
       '<div>' +
-        '<table class="origin-table">' +
-          '<tbody>' +
-
-            '<tr>' +
-              '<th scope="row">Magnitude</th>' +
-              '<td>' +
-                _this.formatter.magnitude(
-                    magnitude,
-                    magnitudeType,
-                    magnitudeError
-                ) +
-              '</td>' +
-            '</tr>' +
-
-            '<tr>' +
-              '<th scope="row">Magnitude</th>' +
-              '<td>' +
-                _this.formatter.location(latitude, longitude) +
-              '</td>' +
-            '</tr>' +
-
-            '<tr>' +
-              '<th scope="row">Magnitude</th>' +
-              '<td>' +
-                _this.formatter.depth(depth, 'km', depthError) +
-              '</td>' +
-            '</tr>' +
-
-            '<tr>' +
-              '<th scope="row">Magnitude</th>' +
-              '<td>' +
-                _this.formatter.depth(depth, 'km', depthError) +
-              '</td>' +
-            '</tr>' +
-
-          '</tbody>' +
-        '</table>' +
+        '<div class="origin-pin-badge">' +
+          '<a href="#phase-data" title="Origin magnitude">' +
+            '<strong class="origin-magnitude">' +
+              magnitude +
+            '</strong>' +
+            '<br />' +
+            '<abbr title="Magnitude type">' +
+              magnitudeType +
+            '</abbr>' +
+          '</a>' +
+        '</div>' +
+        '<div class="origin-review-status">' +
+          '<strong>' +
+            reviewStatus +
+          '</strong>' +
+          '<br />' +
+          '<abbr title="Review Status">status</abbr>' +
+        '</div>' +
       '</div>';
   };
 
