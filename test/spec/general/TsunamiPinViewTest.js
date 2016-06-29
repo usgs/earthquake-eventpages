cc3ad32eab4c5b135b19005f6f4621b6e2fb766f
Eric Martinez <emartinez@usgs.gov>
2016-06-29 16:21:56 -0600

    More tsunami tests.

--- test/spec/general/TsunamiPinViewTest.js
+++ test/spec/general/TsunamiPinViewTest.js
@@ -27,4 +27,45 @@ describe('general/TsunamiPinView', function () {
       view.destroy(); // Catches double destroy errors
     });
   });
+
+  describe('getLinkUrl', function () {
+    it('returns a tsunami link', function () {
+      var result,
+          view;
+
+      view = TsunamiPinView();
+      result = view.getLinkUrl();
+
+      expect(result).to.equal('http://www.tsunami.gov/');
+
+      view.destroy();
+    });
+  });
+
+  describe('renderPinContent', function () {
+    it('includes an image and some text', function () {
+      var view;
+
+      view = TsunamiPinView();
+      view.renderPinContent();
+
+      expect(view.content.querySelector('img')).to.not.equal(null);
+      expect(view.content.querySelector('.disclaimer')).to.not.equal(null);
+
+      view.destroy();
+    });
+  });
+
+  describe('renderPinFooter', function () {
+    it('creates a link', function () {
+      var view;
+
+      view = TsunamiPinView();
+      view.renderPinFooter();
+
+      expect(view.footer.querySelector('a')).to.not.equal(null);
+
+      view.destroy();
+    });
+  });
 });
