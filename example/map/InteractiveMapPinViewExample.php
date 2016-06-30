ee898a48afa619d663e53f5464b04f0cb804e1b1
Hal Simpson <hasimpson@usgs.gov>
2016-06-30 10:29:38 -0600

    Added InteractiveMapPinViewExample.php

---
+++ example/map/InteractiveMapPinViewExample.php
@@ -0,0 +1,33 @@
+<?php
+if (!isset($TEMPLATE)) {
+
+  $TITLE = 'InteractiveMapPinView';
+
+  // If you want to include section navigation.
+  // The nearest _navigation.inc.php file will be used by default
+  $NAVIGATION = true;
+
+  // Stuff that goes at the top of the page (in the <head>) (i.e. <link> tags)
+  $HEAD = '
+    <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css"/>
+    <link rel="stylesheet" href="/css/event.css"/>
+    <style>
+      .map-pin-view-example {
+        width: 210px;
+        height: 310px;
+      }
+    </style>
+  ';
+
+  // Stuff that goes at the bottom of the page (i.e. <script> tags)
+  $FOOT = '
+    <script src="/js/classes.js"></script>
+    <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
+    <script src="InteractiveMapPinViewExample.js"></script>
+  ';
+
+  include 'template.inc.php';
+}
+?>
+
+<div class="map-pin-view-example"></div>
