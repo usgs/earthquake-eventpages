8a08da18be96eeededdedaf5f8b9e735ea83fdbc
Hal Simpson <hasimpson@usgs.gov>
2016-06-30 14:42:58 -0600

    Refactored to reflect changes to BasePinView.

--- src/htdocs/js/map/InteractiveMapPinView.js
+++ src/htdocs/js/map/InteractiveMapPinView.js
@@ -1,13 +1,15 @@
 'use strict';
 
+
 var BasicPinView = require('core/BasicPinView'),
     InteractiveMapView = require('map/InteractiveMapView'),
-    Module = require('core/Module'),
     Util = require('util/Util');
+
 
 var _DEFAULTS = {
   module: {ID: 'map', TITLE: 'Interactive Map'}
 };
+
 
 var MapPinView = function (options) {
   var _this,
@@ -16,65 +18,59 @@ var MapPinView = function (options) {
       _mapView;
 
   options = Util.extend({}, _DEFAULTS, options);
-
   _this = BasicPinView(options);
 
-  _initialize = function (options) {
-    var buf,
-        el;
+  /**
+   * Initialize the new module.
+   *
+   * @param options {Object}
+   */
+  _initialize = function (/*options*/) {
+    var el;
 
-    _this.module = options.module;
-    _this.model.set({'map':{'ShakeMap MMI Contours':false}}, {'silent':true});
-
-    buf = [];
-    buf.push(
-      '<figure>' +
-        '<a href="#map" class="locationview-map">' +
-          '<div></div>' +
-        '</a>' +
-      '</figure>'
+    // Turn off the shakemap contours.
+    _this.model.set(
+        {'map':{'ShakeMap MMI Contours':false}},
+        {'silent':true}
       );
 
     el = document.createElement('div');
-    el.classList.add('location-view');
-    el.innerHTML = buf.join('');
+    el.classList.add('locationview-map');
 
     _this.content.appendChild(el);
 
     _mapView = InteractiveMapView({
-      el: el.querySelector('.locationview-map > div'),
+      el: el,
       interactive: false,
       model: _this.model,
-      module: Module(),
       scaleControl: false
     });
-    _mapView.el.addEventListener('click', _this.onClick);
   };
 
   _this.destroy = Util.compose(function () {
     if (_this === null) {
       return;
     }
-    _mapView.el.removeEventListerner('click', _this.onClick);
     _mapView.destroy();
     _this = null;
   }, _this.destroy);
-
-  _this.onClick = function () {
-    window.location = '#map';
-  };
 
   _this.renderPinContent = function () {
     _mapView.render();
   };
 
+  /**
+   * Override base function.
+   * This pin doesn't have a contributor.
+   */
   _this.renderPinFooter = function () {
-
   };
+
 
   _initialize(options);
   options = null;
   return _this;
 };
+
 
 module.exports = MapPinView;
