6600ffa15652e2b531c355d07be25967cc2b50e1
Hal Simpson <hasimpson@usgs.gov>
2016-06-30 10:32:24 -0600

    Fixed DYFI example not having leaflet

--- example/dyfi/DYFIViewExample.php
+++ example/dyfi/DYFIViewExample.php
@@ -9,12 +9,14 @@ if (!isset($TEMPLATE)) {
 
   // Stuff that goes at the top of the page (in the <head>) (i.e. <link> tags)
   $HEAD = '
+    <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css"/>
     <link rel="stylesheet" href="/css/event.css"/>
   ';
 
   // Stuff that goes at the bottom of the page (i.e. <script> tags)
   $FOOT = '
     <script src="/js/classes.js"></script>
+    <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
     <script src="DYFIViewExample.js"></script>
   ';
 
