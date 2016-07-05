4456e47527737c024a84200bd75bf3bf2c069356
ehunter-usgs <ehunter@usgs.gov>
2016-07-05 17:22:26 -0600

    Add number of responses to DYFI Tell Us Pin

--- src/htdocs/js/dyfi/DYFIFormPinView.js
+++ src/htdocs/js/dyfi/DYFIFormPinView.js
@@ -3,6 +3,7 @@
 
 var BasicPinView = require('core/BasicPinView'),
     DYFIFormModule = require('dyfi/DYFIFormModule'),
+    Formatter = require('core/Formatter'),
     Util = require('util/Util');
 
 var _DEFAULTS = {
@@ -11,21 +12,47 @@ var _DEFAULTS = {
 
 
 var DYFIFormPinView = function (options) {
-  var _this;
+  var _this,
+      _initialize,
+
+      _formatter;
 
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = BasicPinView(options);
+
+  _initialize = function (options) {
+    _formatter = options.formatter || Formatter();
+  };
+
+  _this.destroy = Util.compose(function () {
+    _formatter = null;
+
+    _initialize = null;
+    _this = null;
+  }, _this.destroy);
 
   /**
    * Render the histograms as DYFIFormPinView content
    *
    */
   _this.renderPinContent = function () {
-    _this.content.innerHTML = 'DYFIFormPinView content';
+    var properties,
+        responses;
+
+    properties = _this.model.get('properties');
+    responses = properties['num-responses'] || properties.numResp;
+
+    _this.content.innerHTML =
+      '<div class="pin-badge dyfi-responses-badge" ' +
+          'title="Number of DYFI Responses">' +
+        '<strong>' + _formatter.number(responses, 0, '&ndash;') + '</strong>' +
+        '<br>' +
+        '<abbr>Responses</abbr>' +
+      '</div>';
   };
 
-
+  _initialize(options);
   options = null;
   return _this;
 };
