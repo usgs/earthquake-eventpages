df6ac826ef2dc0c808a227bfc8b57b648ad3e8d7
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-07-05 09:26:28 -0600

    added beachballview

--- src/htdocs/js/moment-tensor/MomentTensorPinView.js
+++ src/htdocs/js/moment-tensor/MomentTensorPinView.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var BasicPinView = require('core/BasicPinView'),
+    BeachBallView = require('moment-tensor/BeachBallView'),
+    Tensor = require('moment-tensor/Tensor'),
     Util = require('util/Util');
 
 var _DEFAULTS = {
+  fillColor: '#6ea8ff',
   module: {ID: 'moment-tensor', TITLE: 'Moment Tensor'}
 };
 
@@ -12,9 +15,27 @@ var MomentTensorPinView = function (options) {
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = BasicPinView(options);
+  _this.fillColor = options.fillColor;
+  _this.tensor = Tensor.fromProduct(_this.model);
 
   _this.renderPinContent = function () {
-    _this.content.innerHTML = '';
+    var beachball;
+
+    _this.content.innerHTML = '<div class="row moment-tensor-pin-beachball">' +
+      '<div class="column one-of-two"></div>' +
+      '<div class="column one-of-two"></div>' +
+    '</div>';
+
+    beachball = _this.content.querySelector('.column + column');
+
+    beachball = BeachBallView({
+      el: beachball,
+      fillColor: _this.fillColor,
+      size: 320,
+      tensor: _this.tensor
+    });
+
+    beachball.render();
   };
 
   options = null;
