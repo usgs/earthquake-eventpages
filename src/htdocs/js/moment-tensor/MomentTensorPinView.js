098fff64d64bd0b03cca18f5b15632ea56477f6f
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-07-06 14:58:31 -0600

    added beachball to MomentTensorPinView

--- src/htdocs/js/moment-tensor/MomentTensorPinView.js
+++ src/htdocs/js/moment-tensor/MomentTensorPinView.js
@@ -2,44 +2,68 @@
 
 var BasicPinView = require('core/BasicPinView'),
     BeachBallView = require('moment-tensor/BeachBallView'),
+    MomentTensorModule = require('moment-tensor/MomentTensorModule'),
     Tensor = require('moment-tensor/Tensor'),
     Util = require('util/Util');
 
+
 var _DEFAULTS = {
   fillColor: '#6ea8ff',
-  module: {ID: 'moment-tensor', TITLE: 'Moment Tensor'}
+  module: MomentTensorModule
 };
 
+
 var MomentTensorPinView = function (options) {
-  var _this;
+  var _this,
+      _initialize,
+
+      _fillColor,
+      _tensor;
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = BasicPinView(options);
-  _this.fillColor = options.fillColor;
-  _this.tensor = Tensor.fromProduct(_this.model);
 
+  _initialize = function (options) {
+    _fillColor = options.fillColor;
+    _tensor = Tensor.fromProduct(_this.model);
+  };
+
+  _this.destroy = Util.compose(function () {
+    _fillColor = null;
+    _tensor = null;
+
+    _initialize = null;
+    _this = null;
+  }, _this.destroy);
+
+  /**
+   * Creats pin content
+   */
   _this.renderPinContent = function () {
     var beachball;
 
-    _this.content.innerHTML = '<div class="row moment-tensor-pin-beachball">' +
-      '<div class="column one-of-two"></div>' +
-      '<div class="column one-of-two"></div>' +
-    '</div>';
+    _this.content.innerHTML = '<div class="moment-tensor-pin-beachball"></div>';
 
-    beachball = _this.content.querySelector('.column + column');
+    beachball = _this.content.querySelector('.moment-tensor-pin-beachball');
 
     beachball = BeachBallView({
       el: beachball,
-      fillColor: _this.fillColor,
+      fillColor: _fillColor,
+      labelPlanes: false,
       size: 320,
-      tensor: _this.tensor
+      tensor: _tensor
     });
 
     beachball.render();
+    beachball.destroy();
+    beachball = null;
   };
 
+
+  _initialize(options);
   options = null;
   return _this;
 };
+
 
 module.exports = MomentTensorPinView;
