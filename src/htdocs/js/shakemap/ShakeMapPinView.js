f9a58cf1857006d82d1f226c15b0f0acebf2c636
ehunter-usgs <ehunter@usgs.gov>
2016-07-08 11:08:08 -0600

    Add impact bubble to ShakeMapPinView header

--- src/htdocs/js/shakemap/ShakeMapPinView.js
+++ src/htdocs/js/shakemap/ShakeMapPinView.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var BasicPinView = require('core/BasicPinView'),
+    Formatter = require('core/Formatter'),
     ShakeMapModule = require('shakemap/ShakeMapModule'),
     Util = require('util/Util');
 
@@ -10,14 +11,23 @@ var _DEFAULTS = {
 };
 
 var ShakeMapPinView = function (options) {
-  var _this;
+  var _this,
+      _initialize,
+
+      _formatter;
+
 
   options = Util.extend({}, _DEFAULTS, options);
   _this = BasicPinView(options);
 
 
+  _initialize = function (options) {
+    _formatter = options.formatter || Formatter();
+  };
+
   /**
    * Renders content for ShakeMapPinView
+   *
    */
   _this.renderPinContent = function () {
     var tvMap;
@@ -35,7 +45,25 @@ var ShakeMapPinView = function (options) {
     _this.content.innerHTML = tvMap;
   };
 
+  /**
+   * Render header for ShakeMapPinView with impact bubble
+   *
+   */
+  _this.renderPinHeader = function () {
+    var display,
+        maxmmi;
 
+    // Use module ID and TITLE to create a link
+    display = _this.module.TITLE;
+    maxmmi = _this.model.get('properties').maxmmi;
+
+    _this.header.innerHTML = [
+      '<a href="', _this.getLinkUrl(), '">', display, '</a>',
+      _formatter.intensity(maxmmi, null)
+    ].join('');
+  };
+
+  _initialize(options);
   options = null;
   return _this;
 };
