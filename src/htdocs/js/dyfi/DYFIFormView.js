f217fc20d339f1c724b5a4397705ee2c136f05e5
ehunter-usgs <ehunter@usgs.gov>
2017-08-31 15:01:20 -0600

    Move privacy statement to top of contact form

--- src/htdocs/js/dyfi/DYFIFormView.js
+++ src/htdocs/js/dyfi/DYFIFormView.js
@@ -225,6 +225,7 @@ var DYFIFormView = function (options) {
     // Handle contact information
     contactContainer.innerHTML = '<legend>Contact Information' +
         ' <small>(optional)</small></legend>';
+    contactContainer.appendChild(disclaimerEl);
     _this.createTextQuestion(contactInfo, contactContainer);
     moreQuestionsEl.appendChild(contactContainer);
 
@@ -253,7 +254,6 @@ var DYFIFormView = function (options) {
       e.preventDefault();
     });
 
-    contactContainer.appendChild(disclaimerEl);
 
     _this.synchQuestionAnswers();
     _this.addQuestionListeners();
