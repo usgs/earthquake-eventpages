9fe8fd9b61ad89b8e8ab3c03e758eb85a7419219
Eric Martinez <emartinez@usgs.gov>
2018-02-14 16:27:43 -0700

    Added EVENT_SERVICE and DELETED_EVENT_SERVICE as environment parameters.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
 
+  EVENT_SERVICE: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail',
+  DELETED_EVENT_SERVICE: 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?includedeleted=true',
   CONTRIBUTOR_SERVICE: 'https://earthquake.usgs.gov/data/comcat/contributor/index.json.php'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,7 @@
 export const environment = {
   production: false,
 
+  EVENT_SERVICE: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail',
+  DELETED_EVENT_SERVICE: 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?includedeleted=true',
   CONTRIBUTOR_SERVICE: 'https://earthquake.usgs.gov/data/comcat/contributor/index.json.php'
 };
