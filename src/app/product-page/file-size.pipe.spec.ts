d5473e57959ff748bd143db590f1bcea64efb2ef
Eric Martinez <emartinez@usgs.gov>
2018-02-15 13:18:17 -0700

    Bringing up coverage for file-size.

--- src/app/product-page/file-size.pipe.spec.ts
+++ src/app/product-page/file-size.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { FileSizePipe } from './file-size.pipe';
 
 describe('FileSizePipe', () => {
+  let pipe;
+
+  beforeEach(() => {
+    pipe = new FileSizePipe()
+  });
+
+
   it('create an instance', () => {
     const pipe = new FileSizePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  describe('transform', () => {
+    it('formats properly', () => {
+      const result = pipe.transform(1536);
+      expect(result).toBe('1.5 KB');
+    });
+
+    it('formats with precision', () => {
+      const result = pipe.transform(1536, 2);
+      expect(result).toBe('1.50 KB');
+    });
   });
 });
