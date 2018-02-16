6244918c9c3cd91f72be02ac0e1c1532ec09091f
Jeremy Fee <jmfee@usgs.gov>
2018-02-15 18:41:27 -0700

    Move BrowserAnimationsModule import to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -23,6 +24,7 @@ import { FormatterService } from './formatter.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
 
     EventPageModule,
