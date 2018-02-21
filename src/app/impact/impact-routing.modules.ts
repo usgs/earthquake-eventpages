4a5d34a473a281016832867115e3b702ecc1fcf3
jdbrown-usgs <jdbrown@usgs.gov>
2018-02-21 08:54:30 -0700

    added imports and routing

--- src/app/impact/impact-routing.modules.ts
+++ src/app/impact/impact-routing.modules.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DyfisummaryComponent } from './dyfisummary/dyfisummary.component';
 import { ImpactComponent } from './impact/impact.component';
+import { PagerSummaryComponent } from './pager-summary/pager-summary.component';
+import { ShakeMapSummaryComponent } from
+    './shake-map-summary/shake-map-summary.component';
 
 
 const impactRoutes: Routes = [
   {
     path: '',
-    component: ImpactComponent
+    component: ImpactComponent,
+    children: [
+      {
+        path: 'dyfisummary',
+        component: DyfisummaryComponent
+      },
+      {
+        path: 'impact',
+        component: ImpactComponent
+      },
+      {
+        path: 'pager-summary',
+        component: PagerSummaryComponent
+      },
+      {
+        path: 'shake-map-summary',
+        component: ShakeMapSummaryComponent
+      }
+    ]
   }
 ];
 
