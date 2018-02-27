efcda9d75f4ac6bb6d9b8110b686043bcc9ee962
ehunter-usgs <ehunter@usgs.gov>
2018-02-27 15:20:25 -0700

    write tests for regional pin component

--- src/app/executive/regional-pin/regional-pin.component.spec.ts
+++ src/app/executive/regional-pin/regional-pin.component.spec.ts
@@ -15,6 +15,12 @@ describe('RegionalPinComponent', () => {
   let component: RegionalPinComponent;
   let fixture: ComponentFixture<RegionalPinComponent>;
 
+  const coordinates = {
+    latitude: 35,
+    longitude: -105,
+    zoom: 16
+  };
+
   beforeEach(async(() => {
     const eventServiceStub = {
       getEvent: jasmine.createSpy('eventService::getEvent'),
@@ -48,5 +54,80 @@ describe('RegionalPinComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+
+
+
+
+
+  describe('createMap', () => {
+    it('should create a map', () => {
+      // check map
+      expect(component.map).not.toBeNull();
+    });
+  });
+
+  describe('createMarker', () => {
+    it('should create a marker', () => {
+      // check marker
+      expect(component.marker).not.toBeNull();
+    });
+  });
+
+  describe('fitMapBounds', () => {
+    it('should center/zoom the map', () => {
+      let latLng,
+          latitude,
+          longitude,
+          mapSpy;
+
+      // set latitude/ longitude
+      latitude = coordinates.latitude;
+      longitude = coordinates.longitude;
+      latLng = L.latLng(latitude, longitude);
+
+      // create map
+      component.createMap();
+
+      // setup spies
+      mapSpy = spyOn(component.map, 'fitBounds');
+
+      // call moveMap
+      component.fitMapBounds(latitude, longitude);
+
+
+      // check results
+      expect(mapSpy).toHaveBeenCalled();
+      expect(mapSpy).toHaveBeenCalledWith( [[ 33, -107 ], [ 37, -103 ]]);
+    });
+  });
+
+  describe('updateMarkerLocation', () => {
+    it('should move the marker on the map', () => {
+      let latLng,
+          latitude,
+          longitude,
+          markerSpy;
+
+      // create marker
+      component.createMarker();
+
+      // set latitude/ longitude
+      latitude = coordinates.latitude;
+      longitude = coordinates.longitude;
+      latLng = L.latLng(latitude, longitude);
+
+      // setup spies
+      markerSpy = spyOn(component.marker, 'setLatLng');
+
+      // call moveMarker
+      component.updateMarkerLocation(latitude, longitude);
+
+      // check results
+      expect(markerSpy).toHaveBeenCalled();
+      expect(markerSpy).toHaveBeenCalledWith(latLng);
+    });
   });
 });
