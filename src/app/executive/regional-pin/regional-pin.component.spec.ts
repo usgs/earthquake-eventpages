50a6086e176bb9b9ab5db06004cc564118e1debb
ehunter-usgs <ehunter@usgs.gov>
2018-02-27 14:02:13 -0700

    Fix tests

--- src/app/executive/regional-pin/regional-pin.component.spec.ts
+++ src/app/executive/regional-pin/regional-pin.component.spec.ts
@@ -1,14 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatListModule
+} from '@angular/material';
+import { MockPipe } from '../../mock-pipe';
 
 import { RegionalPinComponent } from './regional-pin.component';
+
+import { EventService } from '../../event.service';
 
 describe('RegionalPinComponent', () => {
   let component: RegionalPinComponent;
   let fixture: ComponentFixture<RegionalPinComponent>;
 
   beforeEach(async(() => {
+    const eventServiceStub = {
+      getEvent: jasmine.createSpy('eventService::getEvent'),
+      getProduct: jasmine.createSpy('eventService::getProduct')
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ RegionalPinComponent ]
+      declarations: [
+        RegionalPinComponent,
+
+        MockPipe('contributorList')
+      ],
+      imports: [
+        MatListModule,
+        MatButtonModule,
+        MatCardModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: EventService, useValue: eventServiceStub }
+      ]
     })
     .compileComponents();
   }));
