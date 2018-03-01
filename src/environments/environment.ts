c69a25068d27ebde4a2bb325a76f90b3d9e38141
ehunter-usgs <ehunter@usgs.gov>
2018-02-28 17:59:41 -0700

    Undo changes to dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@
 export const environment = {
   production: false,
 
+
   CONTRIBUTOR_SERVICE: 'https://earthquake.usgs.gov/data/comcat/contributor/index.json.php',
   DELETED_EVENT_SERVICE: 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?includedeleted=true',
   EVENT_SERVICE: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail',
