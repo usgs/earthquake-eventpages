2c343bbf40e6ab36073627e4d33f1664410f74ff
Eric Martinez <emartinez@usgs.gov>
2018-03-06 14:35:59 -0700

    Added some tests.

--- src/app/event-page/navigation/navigation.component.spec.ts
+++ src/app/event-page/navigation/navigation.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MockComponent } from 'ng2-mock-component';
 
+import { Event } from '../../event';
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -28,5 +29,48 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getKmlLink', () => {
+    it('uses event.id in link', () => {
+      const event = new Event({id: 'event'});
+      const link = component.getKmlLink(event);
+
+      expect(link).toBe('/earthquakes/feed/v1.0/detail/event.kml');
+     });
+  });
+
+  describe('getLatestEarthquakesLink', () => {
+    let mapPositionSpy;
+
+    beforeEach(() => {
+      mapPositionSpy = spyOn(component, 'getMapPosition').and.returnValue({});
+    });
+
+     const parseSettings = function (link: string) {
+       const parts = link.split('#');
+       const settings = JSON.parse(decodeURIComponent(parts[1]));
+
+       return settings;
+     };
+
+
+    it('uses current map position', () => {
+      const params = {};
+      const link = component.getLatestEarthquakesLink('eventid', params);
+      const settings = parseSettings(link);
+
+      expect(mapPositionSpy).toHaveBeenCalled();
+      expect(mapPositionSpy).toHaveBeenCalledWith(params);
+      expect(settings.feed).toEqual('eventid');
+    });
+
+    it('works without parameters', () => {
+      const link = component.getLatestEarthquakesLink();
+      const settings = parseSettings(link);
+
+      expect(Math.abs(settings.feed - (new Date()).getTime())).toBeLessThan(1000);
+      expect(settings.mapposition).toEqual({});
+    });
   });
 });
