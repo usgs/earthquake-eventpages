e71f1fd07cbc21beb4f36508782bb12f2fa1d3d8
jdbrown-usgs <jdbrown@usgs.gov>
2018-03-07 14:39:03 -0700

    added component to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,27 +3,32 @@ import { CommonModule } from '@angular/common';
 import { TextProductComponent } from './text-product/text-product.component';
 import { LinkProductComponent } from './link-product/link-product.component';
 import { DownloadDialogComponent } from './download-dialog/download-dialog.component';
-import { MatDialogModule } from '@angular/material';
+import { MatDialogModule, MatIconModule } from '@angular/material';
 import { BeachballComponent } from './beachball/beachball.component';
 import { ProductAttributionComponent } from './product-attribution/product-attribution.component';
+import { PreferredCheckComponent } from './preferred-check/preferred-check.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
   ],
   declarations: [
     BeachballComponent,
     DownloadDialogComponent,
     LinkProductComponent,
     ProductAttributionComponent,
-    TextProductComponent
+    TextProductComponent,
+    PreferredCheckComponent
   ],
   exports: [
     BeachballComponent,
     DownloadDialogComponent,
     LinkProductComponent,
+    MatIconModule,
     ProductAttributionComponent,
+    PreferredCheckComponent,
     TextProductComponent
   ],
   entryComponents: [
