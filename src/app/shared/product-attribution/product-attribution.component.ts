1bd0627c117602c70a27e977cc588d04ca6d6349
Eric Martinez <emartinez@usgs.gov>
2018-03-08 08:58:02 -0700

    Refactor product-attribution to re-use shared-attribution.

--- src/app/shared/product-attribution/product-attribution.component.ts
+++ src/app/shared/product-attribution/product-attribution.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { EventService } from '../../event.service';
-import { ContributorService } from '../../contributor.service';
 
 @Component({
   selector: 'shared-product-attribution',
@@ -11,22 +9,17 @@ export class ProductAttributionComponent implements OnInit {
 
   @Input() product: any;
 
-  constructor(
-    public contributorService: ContributorService,
-    public eventService: EventService
-  ) { }
+  constructor () { }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
-  getSources(product: any, event: any = null, details: Array<any> = []): Array<any> {
-    const sources = new Set<any>();
+  getSources(product: any): Array<string> {
+    const sources = new Set<string>();
 
     if (!product) {
       return [];
     }
-
-    const eventSources = (event && event.sources) ? event.sources : [];
 
     if (product.source) {
       sources.add(product.source.toLowerCase());
@@ -44,23 +37,7 @@ export class ProductAttributionComponent implements OnInit {
       });
     }
 
-    let sourceArray = Array.from(sources);
-    sourceArray.sort();
-
-    sourceArray = sourceArray.map((id) => {
-      return {
-        id: id.toUpperCase(),
-        index: eventSources.indexOf(id) + 1,
-        details: details.find((item: any) => {
-          return (
-            item.id === id ||
-            (item.aliases && item.aliases.indexOf(id) !== -1)
-          );
-        })
-      };
-    });
-
-    return sourceArray;
+    return Array.from(sources);
   }
 
 }
