8fe5ce0acb9744ae69f1218e9dfe90dc601d9a88
Eric Martinez <emartinez@usgs.gov>
2018-03-08 11:04:40 -0700

    Added tests for geoserve service.

--- src/app/geoserve.service.spec.ts
+++ src/app/geoserve.service.spec.ts
@@ -3,6 +3,8 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { GeoserveService } from './geoserve.service';
 
+import { environment } from '../environments/environment';
+
 
 describe('GeoserveService', () => {
   let httpClient: HttpTestingController,
@@ -28,4 +30,32 @@ describe('GeoserveService', () => {
   it('should be created', inject([GeoserveService], (service: GeoserveService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('fe', () => {
+    it('defers to regions method', inject([GeoserveService], (service: GeoserveService) => {
+      const spy = spyOn(service, 'regions').and.returnValue('regions');
+      const result = service.fe(0, 1);
+
+      expect(spy).toHaveBeenCalled();
+      expect(spy).toHaveBeenCalledWith(0, 1, 'fe');
+    }));
+  });
+
+  describe('regions', () => {
+    let stub;
+
+    beforeEach(() => {
+      stub = `${environment.GEOSERVE_SERVICE}/regions.json`;
+    });
+
+    it('injects type of present', inject([GeoserveService], (service: GeoserveService) => {
+      service.regions(0, 1, 'foo').subscribe(() => {});
+      httpClient.expectOne(`${stub}?latitude=0&longitude=1&type=foo`);
+    }));
+
+    it('skips type if not present', inject([GeoserveService], (service: GeoserveService) => {
+      service.regions(0, 1).subscribe();
+      httpClient.expectOne(`${stub}?latitude=0&longitude=1`);
+    }));
+  });
 });
