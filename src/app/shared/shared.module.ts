c6bdfc0656e05a5f0ab64a313b1af1dc3902c652
jdbrown-usgs <jdbrown@usgs.gov>
2018-03-08 14:51:11 -0700

    cleaned up code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatDialogModule, MatIconModule } from '@angular/material';
 
 import { AttributionComponent } from './attribution/attribution.component';
-import { TextProductComponent } from './text-product/text-product.component';
-import { LinkProductComponent } from './link-product/link-product.component';
-import { DownloadDialogComponent } from './download-dialog/download-dialog.component';
-import { MatDialogModule, MatIconModule } from '@angular/material';
 import { AlertLevelComponent } from './alert-level/alert-level.component';
 import { BeachballComponent } from './beachball/beachball.component';
 import { CoordinatesComponent } from './coordinates/coordinates.component';
@@ -14,7 +10,6 @@ import { DownloadDialogComponent } from './download-dialog/download-dialog.compo
 import { FeRegionComponent } from './fe-region/fe-region.component';
 import { LinkProductComponent } from './link-product/link-product.component';
 import { MmiComponent } from './mmi/mmi.component';
-import { ProductAttributionComponent } from './product-attribution/product-attribution.component';
 import { PreferredCheckComponent } from './preferred-check/preferred-check.component';
 import { ProductAttributionComponent } from './product-attribution/product-attribution.component';
 import { TextProductComponent } from './text-product/text-product.component';
@@ -52,7 +47,7 @@ import { BubbleComponent } from './bubble/bubble.component';
   ],
   exports: [
     AttributionComponent,
-    AlertLevelComponent
+    AlertLevelComponent,
     BeachballComponent,
     BubbleComponent,
     CoordinatesComponent,
