772ac2577828b614d9807a9aceed06b23743ae64
dslosky-usgs <dslosky@usgs.gov>
2018-03-09 02:51:13 -0700

    Exporting components and services working

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export { EventService }    from './src/app/event.service';
 export { StationListComponent } from './src/app/shakemap/station-list/station-list.component';
+export { StationComponent } from './src/app/shared/station/station.component';
+export { StationService } from './src/app/station.service';
