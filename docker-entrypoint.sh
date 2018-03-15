1ab71f7362611e52c0fdfc4a5df7e7cb82dd48aa
Eric Martinez <emartinez@usgs.gov>
2018-03-15 11:19:33 -0600

    Fixed sed/ln for base_href with slashes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -99,13 +99,15 @@ EO_HEALTHCHECK
 chmod +x ${HEALTHCHECK_SCRIPT};
 
 # Update bundles to be BASE_HREF aware
-sed "s/BASE_HREF/${BASE_HREF}/" \
+SED_SANITIZED_HREF=${BASE_HREF//\//\\/};
+sed "s/BASE_HREF/${SED_SANITIZED_HREF}/" \
   ${DOCUMENT_ROOT}/BASE_HREF/index.html \
   > ${DOCUMENT_ROOT}/BASE_HREF/index-base-href.html;
 
 mv ${DOCUMENT_ROOT}/BASE_HREF/index-base-href.html \
   ${DOCUMENT_ROOT}/BASE_HREF/index.html
 
+mkdir -p ${DOCUMENT_ROOT}/$(dirname ${BASE_HREF});
 ln -sf ${DOCUMENT_ROOT}/BASE_HREF ${DOCUMENT_ROOT}/${BASE_HREF};
 
 
