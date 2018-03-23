53625096e8c11d47a54733132ecbf400276fd964
dslosky-usgs <dslosky@usgs.gov>
2018-03-23 10:12:25 -0600

    Fix 'rxjs/subscription' import

--- src/app/shakemap/uncertainty/uncertainty.component.ts
+++ src/app/shakemap/uncertainty/uncertainty.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventService } from '../../core/event.service';
-import { Subscription } from 'rxjs/subscription';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-uncertainty',
