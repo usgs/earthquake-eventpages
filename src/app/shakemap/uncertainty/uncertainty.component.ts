fc49d6cf74167e9c42c93998969a903db7d1f8d9
dslosky-usgs <dslosky@usgs.gov>
2018-03-23 04:39:53 -0600

    Uncertainty component working

---
+++ src/app/shakemap/uncertainty/uncertainty.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventService } from '../../core/event.service';
+import { Subscription } from 'rxjs/subscription';
+
+@Component({
+  selector: 'app-uncertainty',
+  templateUrl: './uncertainty.component.html',
+  styleUrls: ['./uncertainty.component.scss']
+})
+export class UncertaintyComponent implements OnInit, OnDestroy {
+  public imageUrl: string = null;
+  public subs = new Subscription();
+
+  constructor (private eventService: EventService) { }
+
+  ngOnInit () {
+    this.subs.add(this.eventService.product$.subscribe((product) => {
+      this.onProduct(product);
+    }));
+  }
+
+  /**
+   * New product, get new image url 
+   *
+   * @param product shakemap product
+   */
+  onProduct (product) {
+    if (product == null ||
+          !product.contents['download/urat_pga.jpg']) {
+      this.imageUrl = null;
+      return;
+    }
+
+    const image = product.contents['download/urat_pga.jpg'];
+    this.imageUrl = image.url;
+  }
+
+  ngOnDestroy () {
+    this.subs.unsubscribe();
+  }
+}
