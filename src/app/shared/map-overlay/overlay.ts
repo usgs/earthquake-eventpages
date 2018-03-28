6ccdeb94ae7dcae46d3b6696645c8c7450fea8c8
ehunter-usgs <ehunter@usgs.gov>
2018-03-28 11:04:27 -0600

    Add interface for overlays

---
+++ src/app/shared/map-overlay/overlay.ts
@@ -0,0 +1,24 @@
+
+import * as L from 'leaflet';
+
+/**
+ * Interface for overlays used with the shared-map component
+ */
+export interface Overlay {
+
+  // optional, bounds of data being displayed in layer (omit for worldwide view)
+  bounds: Array<any>;
+
+  // whether layer should be shown
+  enabled: boolean;
+
+  // layer object
+  layer: L.Layer;
+
+  // legend content for layer object
+  legend: string;
+
+  // title of layer, for layer control
+  title: string;
+
+}
