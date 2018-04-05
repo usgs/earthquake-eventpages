a8ae9d3cda10bf860dd2367738eba75547ae0fd5
jdbrown-usgs <jdbrown@usgs.gov>
2018-04-05 10:58:24 -0600

    created basic pin outline whith title and link

--- src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
+++ src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-dyfi-response-submit-pin',
+  selector: 'executive-dyfi-response-submit-pin',
   templateUrl: './dyfi-response-submit-pin.component.html',
   styleUrls: ['./dyfi-response-submit-pin.component.scss']
 })
 export class DyfiResponseSubmitPinComponent implements OnInit {
 
+  public link = '../tellus';
+  @Input() product: any;
+  public title = 'Felt Report - Tell Us!';
   constructor() { }
 
   ngOnInit() {
