f85874043d4407a0f8cb3a073551632a3e63fcff
jdbrown-usgs <jdbrown@usgs.gov>
2018-04-09 13:45:06 -0600

    created dyfi pin

--- src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
+++ src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { FormatterService } from '../../core/formatter.service';
+
 @Component({
   selector: 'executive-dyfi-response-submit-pin',
   templateUrl: './dyfi-response-submit-pin.component.html',
@@ -7,10 +9,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DyfiResponseSubmitPinComponent implements OnInit {
 
-  public link = '../tellus';
   @Input() product: any;
+
+  public link = '../tellus';
   public title = 'Felt Report - Tell Us!';
-  constructor() { }
+  public footer = 'Citizen Scientist Contributions';
+
+  constructor(
+    public formatterService: FormatterService
+  ) { }
 
   ngOnInit() {
   }
