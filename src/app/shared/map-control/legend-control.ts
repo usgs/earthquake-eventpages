2a505814f3c1e24e0274b6ba62e558a539c50c32
ehunter-usgs <ehunter@usgs.gov>
2018-04-09 16:27:26 -0600

    build Legend Control element and query select out the toggle buttons. Only create container if it does not exist

--- src/app/shared/map-control/legend-control.ts
+++ src/app/shared/map-control/legend-control.ts
@@ -4,52 +4,39 @@ import * as L from 'leaflet';
 const LegendControl = L.Control.extend({
 
     onAdd: function(map) {
-      let className,
-          closeButton,
-          container,
-          legends,
-          link;
+      const className = 'leaflet-control-legend';
 
-      className = 'leaflet-control-legend';
+      if (!this._container) {
+        this._container = L.DomUtil.create('div', className);
+        this._container.innerHTML =
+            '<a href="#" class="' + className + '-show material-icons"' +
+                ' title="Legend">&#xE0DA;</a>' +
+            '<button class="' + className + '-hide">CLOSE</button>';
 
-      closeButton = this._closeButton =
-          L.DomUtil.create('button', className + '-close');
-      closeButton.innerHTML = 'CLOSE';
-      container = this._container =
-          L.DomUtil.create('div', className);
-      legends = this._legends =
-          L.DomUtil.create('ul', className + '-list no-style vertical-scrolling');
-      link = this._link =
-          L.DomUtil.create('a', className + '-toggle material-icons');
-      link.href = '#';
-      link.title = 'Legend';
-      link.innerHTML = '&#xE0DA;';
+        // Makes this work on IE10 Touch devices by stopping it from firing
+        // a mouseout event when the touch is released
+        this._container.setAttribute('aria-haspopup', true);
+      }
 
-
-      // add elements to control
-      container.appendChild(link);
-      container.appendChild(closeButton);
-
-      // Makes this work on IE10 Touch devices by stopping it from firing
-      // a mouseout event when the touch is released
-      container.setAttribute('aria-haspopup', true);
+      this._showButton = this._container.querySelector('.' + className + '-show');
+      this._hideButton = this._container.querySelector('.' + className + '-hide');
 
       if (L.Browser.touch) {
-        L.DomEvent.disableClickPropagation(container);
+        L.DomEvent.disableClickPropagation(this._container);
 
       } else {
         L.DomEvent
-          .disableClickPropagation(container)
-          .disableScrollPropagation(container);
+          .disableClickPropagation(this._container)
+          .disableScrollPropagation(this._container);
       }
 
-      L.DomEvent.on(container, 'mousewheel', L.DomEvent.stopPropagation);
-      L.DomEvent.on(link, 'click', L.DomEvent.stop)
-          .on(link, 'click', this._expand, this);
-      L.DomEvent.on(closeButton, 'click', this._collapse, this);
+      L.DomEvent.on(this._container, 'mousewheel', L.DomEvent.stopPropagation);
+      L.DomEvent.on(this._showButton, 'click', L.DomEvent.stop)
+          .on(this._showButton, 'click', this._open, this);
+      L.DomEvent.on(this._hideButton, 'click', this._close, this);
 
-      // return container
-      return container;
+      // return this._container
+      return this._container;
     },
 
     onRemove: function(map) {
@@ -57,20 +44,20 @@ const LegendControl = L.Control.extend({
     },
 
     /**
-     * Collapse the expanded Legend control
+     * Collapse the Legend control
      *
      */
-    _collapse: function () {
+    _close: function () {
       this._container.className = this._container.className
-          .replace(' leaflet-control-legend-expanded', '');
+          .replace(' leaflet-control-legend-visible', '');
     },
 
     /**
-     * Expand the collapsed Legend control
+     * Expand the Legend control
      *
      */
-    _expand: function () {
-      L.DomUtil.addClass(this._container, 'leaflet-control-legend-expanded');
+    _open: function () {
+      L.DomUtil.addClass(this._container, 'leaflet-control-legend-visible');
     },
 });
 
