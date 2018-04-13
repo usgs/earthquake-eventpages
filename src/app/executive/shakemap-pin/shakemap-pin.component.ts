baa5d9d9e4133744c397257bf1c90a2f03e8041f
dslosky-usgs <dslosky@usgs.gov>
2018-04-12 21:01:52 -0600

    Shakemap pin init

---
+++ src/app/executive/shakemap-pin/shakemap-pin.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'executive-shakemap-pin',
+  templateUrl: './shakemap-pin.component.html',
+  styleUrls: ['./shakemap-pin.component.scss']
+})
+export class ShakemapPinComponent implements OnInit {
+
+  public readonly link = '../shakemap';
+  public readonly title = 'ShakeMap';
+
+  public _event: any;
+  public product: any;
+
+  @Input()
+  set event(event) {
+    this._event = event;
+
+    if (event) {
+      this.product = event.getProduct('shakemap');
+    }
+  }
+  
+  get event() {
+    return this._event;
+  }
+
+  constructor () { }
+
+
+  ngOnInit () {
+  }
+
+}
