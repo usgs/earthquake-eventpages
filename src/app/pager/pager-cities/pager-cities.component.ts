25cb06471c3a4b52b0d16e9080906fa982cb1a1d
ehunter-usgs <ehunter@usgs.gov>
2018-05-03 17:11:04 -0600

    Create PagerCitiesComponent

--- src/app/pager/pager-cities/pager-cities.component.ts
+++ src/app/pager/pager-cities/pager-cities.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'pager-cities',
   templateUrl: './pager-cities.component.html',
   styleUrls: ['./pager-cities.component.scss']
 })
-export class PagerCitiesComponent implements OnInit {
+export class PagerCitiesComponent {
+  @ViewChild('citiesTable', {read: ElementRef}) tableEl: ElementRef;
+
+  public columnsToDisplay = [
+    'mmi',
+    'city',
+    'population'
+  ];
 
   @Input() pager;
 
   constructor() { }
 
-  ngOnInit() {
+  toggleCities () {
+    let el;
+
+    const className = 'show';
+    el = this.tableEl.nativeElement;
+
+    if (el.classList.contains(className)) {
+      el.classList.remove(className);
+    } else {
+      el.classList.add(className);
+    }
   }
 
 }
--- src/app/pager/pager.module.ts
+++ src/app/pager/pager.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule, MatTableModule } from '@angular/material';
 import { ProductPageModule } from '../product-page/product-page.module';
 import { PagerComponent } from './pager/pager.component';
 import { PagerRoutingModule } from './pager-routing.module';
@@ -14,6 +14,7 @@ import { PagerCitiesComponent } from './pager-cities/pager-cities.component';
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
     MatTableModule,
     ProductPageModule,
     SharedModule,
