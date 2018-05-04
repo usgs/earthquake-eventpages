73d6499ca0f7e8870ef7fca771b9ec4acbb38865
ehunter-usgs <ehunter@usgs.gov>
2018-05-04 11:17:37 -0600

    Add test for toggling entire list

--- src/app/pager/pager-cities/pager-cities.component.spec.ts
+++ src/app/pager/pager-cities/pager-cities.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCheckboxModule, MatTableModule } from '@angular/material';
 import { MockComponent } from 'ng2-mock-component';
+import { ElementRef } from '@angular/core';
 
 import { PagerCitiesComponent } from './pager-cities.component';
 import { MockPipe } from '../../mock-pipe';
@@ -34,5 +35,18 @@ describe('PagerCitiesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onChange', () => {
+    it('should toggle the full table', () => {
+      // create element reference
+      component.tableEl = new ElementRef(document.createElement('table'));
+
+      component.onChange({ checked: true });
+      expect(component.tableEl.nativeElement.classList.contains('show')).toBeTruthy();
+
+      component.onChange({ checked: false });
+      expect(component.tableEl.nativeElement.classList.contains('show')).toBeFalsy();
+    });
   });
 });
