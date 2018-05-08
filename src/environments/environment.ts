c99861c85c53775046062fb3d3b3a11cd1ebbf63
Jonathan Brown <jdbrown@usgs.gov>
2018-05-08 14:06:24 -0600

    updated text

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
 
   CONTRIBUTOR_SERVICE: 'https://earthquake.usgs.gov/data/comcat/contributor/index.json.php',
   DELETED_EVENT_SERVICE: 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?includedeleted=true',
-  EVENT_SERVICE: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail',
+  EVENT_SERVICE: 'https://dev-earthquake.cr.usgs.gov/earthquakes/feed/v1.0/detail',
   GEOSERVE_SERVICE: 'https://earthquake.usgs.gov/ws/geoserve'
 };
