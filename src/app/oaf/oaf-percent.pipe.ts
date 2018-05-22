834fb93950320952b165dcaf7f1cbbe9a9dd4ef1
Jonathan Brown <jdbrown@usgs.gov>
2018-05-22 08:45:48 -0600

    updated pipe to be used by commentary and forecast tabs

--- src/app/oaf/oaf-percent.pipe.ts
+++ src/app/oaf/oaf-percent.pipe.ts
@@ -3,25 +3,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'oafPercent'
 })
+
 export class OafPercentPipe implements PipeTransform {
 
   /**
    * Formats decimal percent value.
    *
-   * @param value {number}
-   *       Number to format
+   * @param value {any}
+   *       Number or string to format
    * @return {string}
    *       Returns formatted number
    */
-  transform(value: number): any {
-    value = Math.round(value * 100);
-
-    if (value > 99) {
-      return '> 99';
-    } else if (value < 1 ) {
-      return '< 1';
-    } else {
-      return value;
+  transform(value: any): any {
+    if (typeof value !== 'number') {
+      value = parseFloat(value);
     }
+
+    if (value > 0.99) {
+      value = '> 99';
+    } else if (value < 0.01) {
+      value = '< 1';
+    } else {
+      value = (value * 100).toFixed(0);
+    }
+
+    return value.toString() + ' %';
   }
 }
