4887e5b84349e422c5f479ffea934e4f359812a2
dslosky-usgs <dslosky@usgs.gov>
2018-06-05 14:00:28 -0600

    Switch to dyfi data

--- src/app/dyfi/dyfi.module.ts
+++ src/app/dyfi/dyfi.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 
-import { NgxChartsModule } from '../shared/ngx-charts/ngx-charts.module';
-
 import { DyfiComponent } from './dyfi/dyfi.component';
 import { DyfiRoutingModule } from './dyfi-routing.module';
 import { IntensityComponent } from './intensity/intensity.component';
 import { IntensityVsDistanceComponent } from './intensity-vs-distance/intensity-vs-distance.component';
 import { ProductPageModule } from '../product-page/product-page.module';
+import { NgxChartsModule } from '../shared/ngx-charts/ngx-charts.module';
 import { ResponsesVsTimeComponent } from './responses-vs-time/responses-vs-time.component';
 import { ResponsesComponent } from './responses/responses.component';
 import { ZipComponent } from './zip/zip.component';
--- src/app/dyfi/intensity-vs-distance/intensity-vs-distance.component.ts
+++ src/app/dyfi/intensity-vs-distance/intensity-vs-distance.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { DyfiService } from '../../core/dyfi.service';
+import { EventService } from '../../core/event.service';
+
 @Component({
   selector: 'dyfi-intensity-vs-distance',
   templateUrl: './intensity-vs-distance.component.html',
   styleUrls: ['./intensity-vs-distance.component.scss']
 })
 export class IntensityVsDistanceComponent implements OnInit {
-  public multi: any[];
-
-  // options
+  private subs = new Subscription();
+  public dyfiSeries = null;
+  
+  // plot options
   showXAxis = true;
   showYAxis = true;
   gradient = false;
@@ -18,120 +24,46 @@ export class IntensityVsDistanceComponent implements OnInit {
   showYAxisLabel = true;
   yAxisLabel = 'Population';
 
-  colorScheme = {
+   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
 
-  // line, area
-  autoScale = true;
-  
-  constructor() {
+   // line, area
+  autoScale = true
 
-    this.multi = [
-  {
-    "name": "Germany",
-    "series": [
-      {
-        "name": "2010",
-        "x": 40632,
-        "y": 80.3,
-        "max": 81,
-        "min": 80,
-        "r": 80.4
-      },
-      {
-        "name": "2000",
-        "x": 36953,
-        "y": 80.3,
-        "r": 78
-      },
-      {
-        "name": "1990",
-        "x": 31476,
-        "y": 75.4,
-        "r": 79
-      }
-    ]
-  },
-  {
-    "name": "USA",
-    "series": [
-      {
-        "name": "2010",
-        "x": 49737,
-        "y": 78.8,
-        "r": 310
-      },
-      {
-        "name": "2000",
-        "x": 45986,
-        "y": 76.9,
-        "r": 283
-      },
-      {
-        "name": "1990",
-        "x": 3706,
-        "y": 75.4,
-        "r": 253
-      }
-    ]
-  },
-  {
-    "name": "France",
-    "series": [
-      {
-        "name": "2010",
-        "x": 36745,
-        "y": 81.4,
-        "r": 63
-      },
-      {
-        "name": "2000",
-        "x": 34774,
-        "y": 79.1,
-        "r": 59.4
-      },
-      {
-        "name": "1990",
-        "x": 29476,
-        "y": 77.2,
-        "r": 56.9
-      }
-    ]
-  },
-  {
-    "name": "United Kingdom",
-    "series": [
-      {
-        "name": "2010",
-        "x": 36240,
-        "y": 80.2,
-        "r": 62.7
-      },
-      {
-        "name": "2000",
-        "x": 32543,
-        "y": 77.8,
-        "r": 58.9
-      },
-      {
-        "name": "1990",
-        "x": 26424,
-        "y": 75.7,
-        "r": 57.1
-      }
-    ]
+  constructor (
+    public dyfiService: DyfiService,
+    public eventService: EventService
+  ) {}
+
+  ngOnInit() {
+    this.subs.add(this.dyfiService.plotAtten$.subscribe((series) => {
+      this.onDyfiSeries(series);
+    }));
+    this.subs.add(this.eventService.product$.subscribe((product) => {
+      this.onProduct(product);
+    }));
   }
-];
 
-    console.log(this)
+  /**
+   * New product, get new station list
+   *
+   * @param product shakemap product
+   */
+  onProduct(product) {
+    this.dyfiService.getAtten(product);
+  }
+
+  onDyfiSeries(series) {
+    if (series == null) {
+      this.dyfiSeries = null;
+    }
+
+    this.dyfiSeries = series;
   }
   
   onSelect(event) {
     console.log(event);
-  }
-  
-  ngOnInit () {
   }
 
 }
