e8213dc050ecca4cb09349f338e1b97e87327939
Eric Martinez <emartinez@usgs.gov>
2018-06-28 10:42:10 -0600

    Testing number with separator pipe.

--- src/app/shared/number-with-separator.pipe.spec.ts
+++ src/app/shared/number-with-separator.pipe.spec.ts
@@ -5,4 +5,33 @@ describe('NumberWithSeparatorPipe', () => {
     const pipe = new NumberWithSeparatorPipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    let pipe;
+
+    beforeEach(() => {
+      pipe = new NumberWithSeparatorPipe();
+    });
+
+    it('deals with bad input', () => {
+      expect(pipe.transform()).toEqual('');
+      expect(pipe.transform(null)).toEqual('');
+    });
+
+    it('deals with good input', () => {
+      expect(pipe.transform(0)).toEqual('0');
+      expect(pipe.transform(0.0)).toEqual('0');
+      expect(pipe.transform(0.1)).toEqual('0.1');
+
+      expect(pipe.transform(3.14159)).toEqual('3.14159');
+      expect(pipe.transform(9)).toEqual('9');
+      expect(pipe.transform(999)).toEqual('999');
+      expect(pipe.transform(123456789)).toEqual('123,456,789');
+
+      expect(pipe.transform(-3.14159)).toEqual('-3.14159');
+      expect(pipe.transform(-9)).toEqual('-9');
+      expect(pipe.transform(-999)).toEqual('-999');
+      expect(pipe.transform(-123456789)).toEqual('-123,456,789');
+    });
+  });
 });
