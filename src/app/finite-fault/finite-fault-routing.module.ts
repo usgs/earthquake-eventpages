776ce45e8ecec29909957736eb2dfdb01f0ffae9
Wilson <cswilson@igskcicgwsgm015.cr.usgs.gov>
2018-06-28 11:28:24 -0600

    added data and results sections to finite fault and formatted numbers/degrees with pipes

--- src/app/finite-fault/finite-fault-routing.module.ts
+++ src/app/finite-fault/finite-fault-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { FiniteFaultComponent } from './finite-fault/finite-fault.component';
 
 const finiteFaultRoutes = [
