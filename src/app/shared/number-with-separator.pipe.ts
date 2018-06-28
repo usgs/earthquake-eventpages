78797247148f16fe1df0ea9c846cb6cf95be2c5a
Eric Martinez <emartinez@usgs.gov>
2018-06-28 10:47:30 -0600

    Correcting implementation to be more robust.

--- src/app/shared/number-with-separator.pipe.ts
+++ src/app/shared/number-with-separator.pipe.ts
@@ -5,12 +5,42 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NumberWithSeparatorPipe implements PipeTransform {
 
-  transform(value: Number, locale?: any): any {
+  transform(value: number, locale?: any): any {
+    let negative = '';
+
     if (typeof value === 'undefined' || value === null) {
       return '';
     }
 
-    return value.toLocaleString(locale);
+    // Swap value around if negative
+    if (value < 0) {
+      negative = '-';
+      value *= -1;
+    }
+
+    const meta = this._getLocaleInfo(locale);
+    const parts = ('' + value).split('.');
+    const wholePart = parts[0];
+    const decimalPart = parts[1];
+
+    if (typeof decimalPart === 'undefined') {
+      // Input was an integer
+      return negative +
+        value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, meta.separator);
+    } else {
+      // Input was a decimal
+      return negative +
+        wholePart.replace(/\B(?=(\d{3})+(?!\d))/g, meta.separator) +
+        meta.decimal + decimalPart;
+    }
   }
 
+  private _getLocaleInfo (locale) {
+    const knownValue = 1000.1;
+    const localeValue = knownValue.toLocaleString(locale);
+    return {
+      separator: localeValue.substr(1, 1),
+      decimal: localeValue.substr(5, 1)
+    };
+  }
 }
