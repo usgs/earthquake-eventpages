84b9ae091185ffc37fc5bf00464797034a9f41ef
dslosky-usgs <dslosky@usgs.gov>
2018-06-29 16:54:49 -0600

    Test passing

--- src/app/dyfi/responses/responses.component.spec.ts
+++ src/app/dyfi/responses/responses.component.spec.ts
@@ -1,14 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { MockComponent } from 'ng2-mock-component';
+import { of } from 'rxjs';
+
+import { DyfiService } from '../dyfi.service';
+import { EventService } from '../../core/event.service';
+import { MockPipe } from '../../mock-pipe';
 import { ResponsesComponent } from './responses.component';
 
 describe('ResponsesComponent', () => {
   let component: ResponsesComponent;
   let fixture: ComponentFixture<ResponsesComponent>;
 
+  const eventServiceStub = {
+    product$: of({})
+  };
+
+  const dyfiServiceStub = {
+    getCdi: () => {},
+    cdiZip$: of({})
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResponsesComponent ]
+      imports: [
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        NoopAnimationsModule
+      ],
+      declarations: [
+        ResponsesComponent,
+
+        MockComponent({selector: 'shared-mmi', inputs: ['intensity']}),
+        MockPipe('sharedUnits')
+      ],
+      providers: [
+        {provide: EventService, useValue: eventServiceStub},
+        {provide: DyfiService, useValue: dyfiServiceStub}
+      ]
     })
     .compileComponents();
   }));
