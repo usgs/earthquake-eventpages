0b760a262d3680ce76977d14f5132e939a7680e9
jdbrown-usgs <jdbrown@usgs.gov>
2018-07-06 13:00:47 -0600

    fixed logic

--- src/app/executive/dyfi-pin/dyfi-pin.component.spec.ts
+++ src/app/executive/dyfi-pin/dyfi-pin.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent } from 'ng2-mock-component';
 
+import { MockPipe } from '../../mock-pipe';
+
 import { DyfiPinComponent } from './dyfi-pin.component';
 
 describe('DyfiPinComponent', () => {
@@ -15,6 +17,8 @@ describe('DyfiPinComponent', () => {
 
         MockComponent({selector: 'basic-pin', inputs: ['link', 'title', 'product']}),
         MockComponent({selector: 'shared-mmi', inputs: ['bubble', 'intensity']}),
+
+        MockPipe('sharedProductContent')
       ],
       imports: [
         RouterTestingModule
