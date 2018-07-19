d02e7fe30e23a1a35af83298f31802d5ce829f63
Wilson <cswilson@igskcicgwsgm015.gs.doi.net>
2018-07-19 10:29:43 -0600

    added shared product pipe to pager pin

--- src/app/executive/pager-pin/pager-pin.component.spec.ts
+++ src/app/executive/pager-pin/pager-pin.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent } from 'ng2-mock-component';
+import {MockPipe} from '../../mock-pipe';
 
 import { PagerPinComponent } from './pager-pin.component';
+
 
 
 describe('PagerPinComponent', () => {
@@ -15,7 +17,8 @@ describe('PagerPinComponent', () => {
         PagerPinComponent,
 
         MockComponent({selector: 'basic-pin', inputs: ['link', 'product', 'title']}),
-        MockComponent({selector: 'shared-alert-level', inputs: ['alert']})
+        MockComponent({selector: 'shared-alert-level', inputs: ['alert']}),
+        MockPipe('sharedProductContent')
       ],
       imports: [
         RouterTestingModule
