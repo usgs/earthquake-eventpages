23750992571fcf86fe5e1513226bbcec1aac172d
ehunter-usgs <ehunter@usgs.gov>
2018-07-25 15:01:37 -0600

    product-page > footer: remove unused code, organize imports, add comments

--- src/app/product-page/footer/footer.component.ts
+++ src/app/product-page/footer/footer.component.ts
@@ -1,17 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * Displays custom product page footer and product downloads
+ *
+ * @param product {Product}
+ *    The product contents to display
+ */
 @Component({
   selector: 'product-page-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent {
   @Input() product: any;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
