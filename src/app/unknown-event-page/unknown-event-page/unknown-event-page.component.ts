fcd5d3041206befa3ce07790cc33471715f437ff
Wilson <cswilson@igskcicgwsgm015.cr.usgs.gov>
2018-07-30 10:13:25 -0600

    finished unkown event app code and removed helper links

--- src/app/unknown-event-page/unknown-event-page/unknown-event-page.component.ts
+++ src/app/unknown-event-page/unknown-event-page/unknown-event-page.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 
+/**
+ * Unkown event component, loads when there is no event information
+ */
 @Component({
   selector: 'app-unknown-event-page',
   templateUrl: './unknown-event-page.component.html',
