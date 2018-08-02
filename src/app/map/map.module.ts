e4f96fd3679a4a8bd1aea221e15c6af5acddfcdc
ehunter-usgs <ehunter@usgs.gov>
2018-08-02 17:09:43 -0600

    Update coding standards for map module

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MapComponent } from './map/map.component';
+import { NgModule } from '@angular/core';
+
 import { SharedModule } from '../shared/shared.module';
+import { MapComponent } from './map/map.component';
+
 
 @NgModule({
   imports: [
