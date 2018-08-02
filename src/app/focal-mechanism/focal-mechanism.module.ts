02591fb730d74800a6989371d4cca7ce627aa0fa
ehunter-usgs <ehunter@usgs.gov>
2018-08-02 16:12:45 -0600

    Update focal mechanism module imports

--- src/app/focal-mechanism/focal-mechanism.module.ts
+++ src/app/focal-mechanism/focal-mechanism.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material';
 
-import { SharedModule } from '../shared/shared.module';
 import { ProductPageModule } from '../product-page/product-page.module';
-
+import { SharedModule } from '../shared/shared.module';
+import { AttributionComponent } from './attribution/attribution.component';
 import { FocalMechanismRoutingModule } from './focal-mechanism-routing.module';
 import { FocalMechanismComponent } from './focal-mechanism/focal-mechanism.component';
-import { AttributionComponent } from './attribution/attribution.component';
+
 
 @NgModule({
   imports: [
