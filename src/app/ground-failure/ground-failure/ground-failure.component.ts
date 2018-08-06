9623c15a870e5420b82101cae9b666431c268e52
ehunter-usgs <ehunter@usgs.gov>
2018-08-06 14:45:47 -0600

    Update ground-failure > ground-failure component

--- src/app/ground-failure/ground-failure/ground-failure.component.ts
+++ src/app/ground-failure/ground-failure/ground-failure.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { EventService } from '../../core/event.service';
 
 
+/**
+ * Display ground-failure product page
+ */
 @Component({
   selector: 'ground-failure',
   templateUrl: './ground-failure.component.html',
