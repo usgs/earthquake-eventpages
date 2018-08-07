e6316e828f178b75b5828ec339db7fe39ca7c1ce
ehunter-usgs <ehunter@usgs.gov>
2018-08-07 17:21:05 -0600

    executive > origin-pin: coding standards

--- src/app/executive/origin-pin/origin-pin.component.spec.ts
+++ src/app/executive/origin-pin/origin-pin.component.spec.ts
@@ -1,17 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import {
   MatButtonModule,
   MatCardModule,
   MatDividerModule,
   MatListModule
 } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { MockComponent } from 'ng2-mock-component';
 
 import { FormatterService } from '../../core/formatter.service';
 import { Event } from '../../event';
-import { OriginPinComponent } from './origin-pin.component';
 import { MockPipe } from '../../mock-pipe';
+import { OriginPinComponent } from './origin-pin.component';
 
 
 describe('OriginPinComponent', () => {
