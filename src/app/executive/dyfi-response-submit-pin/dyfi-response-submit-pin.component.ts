7c14da5b9f4f667ee4a5f34ba229e86ef184e2f0
ehunter-usgs <ehunter@usgs.gov>
2018-08-07 15:58:22 -0600

    executive > dyfi-response-submit-pin: coding standards

--- src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
+++ src/app/executive/dyfi-response-submit-pin/dyfi-response-submit-pin.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+
+/**
+ * DYFI Response Pin
+ *
+ * @param product
+ *     dyfi product
+ */
 @Component({
   selector: 'executive-dyfi-response-submit-pin',
   templateUrl: './dyfi-response-submit-pin.component.html',
   styleUrls: ['./dyfi-response-submit-pin.component.scss']
 })
-export class DyfiResponseSubmitPinComponent implements OnInit {
-
-  @Input() product: any;
+export class DyfiResponseSubmitPinComponent {
 
   public link = '../tellus';
   public title = 'Felt Report - Tell Us!';
   public footer = 'Citizen Scientist Contributions';
 
-  constructor () { }
-
-  ngOnInit () { }
+  @Input() product: any;
 
 }
