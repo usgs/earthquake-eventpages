e5f3532e99d5a4f23c0449927bd4e60445f76234
ehunter-usgs <ehunter@usgs.gov>
2018-08-07 16:37:14 -0600

    executive > focal-mechanism-pin: coding standards

--- src/app/executive/focal-mechanism-pin/focal-mechanism-pin.component.ts
+++ src/app/executive/focal-mechanism-pin/focal-mechanism-pin.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { Event } from '../../event';
 import { Tensor } from '../../shared/beachball/tensor';
 
 
+/**
+ * Focal Mechanism Pin
+ *
+ * @param product
+ *     focal-mechanmism pin
+ */
 @Component({
   selector: 'executive-focal-mechanism-pin',
   templateUrl: './focal-mechanism-pin.component.html',
   styleUrls: ['./focal-mechanism-pin.component.scss']
 })
-export class FocalMechanismPinComponent implements OnInit {
+export class FocalMechanismPinComponent {
 
-  private _product: any = null;
-
-  public link = '../focal-mechanism';
-  public title = 'Focal Mechanism';
+  public _product: any = null;
+  public link: String = '../focal-mechanism';
+  public title: String = 'Focal Mechanism';
   public tensor: Tensor = null;
 
   @Input() set product (product: any) {
@@ -24,9 +30,5 @@ export class FocalMechanismPinComponent implements OnInit {
   get product () {
     return this._product;
   }
-
-  constructor () { }
-
-  ngOnInit () { }
 
 }
