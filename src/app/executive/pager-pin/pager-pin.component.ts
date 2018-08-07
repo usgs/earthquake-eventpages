029b4ede01f8588d6332a3eb0a17abddb8897430
ehunter-usgs <ehunter@usgs.gov>
2018-08-07 17:23:46 -0600

    executive > pager-pin: coding standards

--- src/app/executive/pager-pin/pager-pin.component.ts
+++ src/app/executive/pager-pin/pager-pin.component.ts
@@ -1,21 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
+/**
+ * Pager Pin
+ *
+ * @param product
+ *     losspager product
+ */
 @Component({
   selector: 'executive-pager-pin',
   templateUrl: './pager-pin.component.html',
   styleUrls: ['./pager-pin.component.scss']
 })
-export class PagerPinComponent implements OnInit {
+export class PagerPinComponent {
 
   public link = '../pager';
   public title = 'PAGER';
 
   @Input() product: any;
-
-  constructor () { }
-
-  ngOnInit () {
-  }
 
 }
