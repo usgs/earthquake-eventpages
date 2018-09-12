6639801c175ae717c605c42f4b45992083e3bca4
jdbrown-usgs <jdbrown@usgs.gov>
2018-09-12 11:51:23 -0600

    created component

--- src/app/technical/finite-fault-summary/finite-fault-summary.component.ts
+++ src/app/technical/finite-fault-summary/finite-fault-summary.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input
+} from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'technical-finite-fault-summary',
   styleUrls: ['./finite-fault-summary.component.scss'],
   templateUrl: './finite-fault-summary.component.html'
 })
-export class FiniteFaultSummaryComponent implements OnInit {
+export class FiniteFaultSummaryComponent {
+  // Table headers
+  columnsToDisplay = [
+    'catalog',
+    'derived-magnitude',
+    'maximum-slip',
+    'source'
+  ];
+
+  @Input()
+  event: any;
+  @Input()
+  products: Array<any>;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
