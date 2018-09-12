8f6a986ffc7a3a886376950f07528a6edcbac581
Eric Martinez <emartinez@usgs.gov>
2018-09-12 08:47:55 -0600

    Linting. Running all tests.

--- src/app/region-info/fetch-nearby-cities.pipe.spec.ts
+++ src/app/region-info/fetch-nearby-cities.pipe.spec.ts
@@ -1,28 +1,9 @@
 import { FetchNearbyCitiesPipe } from './fetch-nearby-cities.pipe';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 describe('FetchNearbyCitiesPipe', () => {
   let pipe, service;
 
   beforeEach(() => {
-    const cities = [
-      {
-        admin1_code: 'NM',
-        admin1_name: 'New Mexico',
-        azimuth: 162.7,
-        country_code: 'US',
-        country_name: 'United States',
-        distance: 2.736,
-        elevation: 1633,
-        feature_class: 'P',
-        feature_code: 'PPL',
-        latitude: 34.97645,
-        longitude: -104.99111,
-        name: 'Vegas Junction',
-        population: 0
-      }
-    ];
-
     service = {
       get: jasmine.createSpy()
     };
@@ -34,7 +15,7 @@ describe('FetchNearbyCitiesPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  fdescribe('transform', () => {
+  describe('transform', () => {
     it('returns as output what was provided as input', () => {
       const obj = {};
       expect(pipe.transform(obj)).toBe(obj);
