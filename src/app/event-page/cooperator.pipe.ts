4aa87547e5f96d943c9127d0ae6f6ee9b6eda059
Eric Martinez <emartinez@usgs.gov>
2018-09-18 19:10:39 -0600

    Added documentation.

--- src/app/event-page/cooperator.pipe.ts
+++ src/app/event-page/cooperator.pipe.ts
@@ -6,6 +6,20 @@ import { Event } from '../event';
   name: 'cooperator'
 })
 export class CooperatorPipe implements PipeTransform {
+  /**
+   * Uses the given event to determine the authoritative contributor, then
+   * looks up that contributors information in the given contributors array
+   * for attribution details. If the contributor is not found or if the
+   * information is incomplete, no attribution is presented.
+   *
+   * @param contributors
+   *      List of contributor information as returned from web service.
+   * @param event
+   *      Current event for which to attribute cooperation
+   *
+   * @return {Cooperator}
+   *      The information used to display attribution by the template.
+   */
   transform(contributors: any[], event: Event): Cooperator {
     try {
       const network = event.properties.net;
