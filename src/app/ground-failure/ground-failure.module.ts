f840b5897afbf05c01de9a4c4bca6eaf11acdac0
ehunter-usgs <ehunter@usgs.gov>
2018-09-20 10:00:39 -0600

    Organize imports in ground-failure.module

--- src/app/ground-failure/ground-failure.module.ts
+++ src/app/ground-failure/ground-failure.module.ts
@@ -5,16 +5,16 @@ import { MatButtonModule } from '@angular/material';
 
 import { MdcTabModule, MdcIconModule } from '@angular-mdc/web';
 
-import { ProductPageModule } from '../product-page/product-page.module';
 import { SharedModule } from '@shared/shared.module';
+import { ProductPageModule } from '../product-page/product-page.module';
 import { AboutComponent } from './about/about.component';
 import { AlertBarComponent } from './alert-bar/alert-bar.component';
 import { GetBarPositionPipe } from './get-bar-position.pipe';
 import { GroundFailureRoutingModule } from './ground-failure-routing.module';
 import { GroundFailureComponent } from './ground-failure/ground-failure.component';
-import { SummaryComponent } from './summary/summary.component';
 import { HazardAlertComponent } from './hazard-alert/hazard-alert.component';
 import { PopulationAlertComponent } from './population-alert/population-alert.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
@@ -28,13 +28,13 @@ import { PopulationAlertComponent } from './population-alert/population-alert.co
   ],
   imports: [
     CommonModule,
+    GroundFailureRoutingModule,
     MatButtonModule,
     MdcIconModule,
     MdcTabModule,
     RouterModule,
-    ProductPageModule,
     SharedModule,
-    GroundFailureRoutingModule
+    ProductPageModule
   ]
 })
 export class GroundFailureModule {}
