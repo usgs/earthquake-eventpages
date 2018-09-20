ab028f4da505f5d1787a047ea443b211ecb1040b
Eric Martinez <emartinez@usgs.gov>
2018-09-20 08:30:05 -0600

    Added some tests to reproduce rounding bug.

--- src/app/shared/significant-figure.pipe.spec.ts
+++ src/app/shared/significant-figure.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { SignificantFigurePipe } from './significant-figure.pipe';
 
-describe('SignificantFigurePipe', () => {
+fdescribe('SignificantFigurePipe', () => {
   it('create an instance', () => {
     const pipe = new SignificantFigurePipe();
     expect(pipe).toBeTruthy();
@@ -9,7 +9,7 @@ describe('SignificantFigurePipe', () => {
     const pipe = new SignificantFigurePipe();
     expect(pipe.transform('1234567', 2)).toEqual(1200000);
   });
-  it('calculates when value is a string', () => {
+  it('calculates when value is a number', () => {
     const pipe = new SignificantFigurePipe();
     expect(pipe.transform(1111111, 1)).toEqual(1000000);
     expect(pipe.transform(1111111, 2)).toEqual(1100000);
@@ -18,5 +18,9 @@ describe('SignificantFigurePipe', () => {
     expect(pipe.transform(1111111, 5)).toEqual(1111100);
     expect(pipe.transform(1111111, 6)).toEqual(1111110);
     expect(pipe.transform(1111111, 7)).toEqual(1111111);
+    expect(pipe.transform(101832.993890020366599, 1)).toEqual(100000);
+    expect(pipe.transform(101832.993890020366599, 3)).toEqual(102000);
+    expect(pipe.transform(101832.993890020366599, 9)).toEqual(101832.994);
+    expect(pipe.transform(0.12345, 1)).toEqual(0.1);
   });
 });
