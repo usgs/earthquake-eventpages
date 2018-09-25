fc16bb8e1f7ea0d065ab96c5df2191581ff21a0f
Eric Martinez <emartinez@usgs.gov>
2018-09-25 08:43:32 -0600

    Fixing/adding tests.

--- src/app/ground-failure/point-source.pipe.spec.ts
+++ src/app/ground-failure/point-source.pipe.spec.ts
@@ -5,4 +5,48 @@ describe('PointSourcePipe', () => {
     const pipe = new PointSourcePipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    it('works as expected', () => {
+      const pipe = new PointSourcePipe();
+
+      expect(pipe.transform(undefined)).toBeFalsy();
+      expect(pipe.transform(null)).toBeFalsy();
+      expect(pipe.transform({})).toBeFalsy();
+      expect(pipe.transform({ properties: {} })).toBeFalsy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': undefined } })
+      ).toBeFalsy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': null } })
+      ).toBeFalsy();
+
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'false' } })
+      ).toBeFalsy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'FALSE' } })
+      ).toBeFalsy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'fAlSe' } })
+      ).toBeFalsy();
+
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'true' } })
+      ).toBeTruthy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'TRUE' } })
+      ).toBeTruthy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'tRuE' } })
+      ).toBeTruthy();
+
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': 'rando' } })
+      ).toBeTruthy();
+      expect(
+        pipe.transform({ properties: { 'rupture-warning': '' } })
+      ).toBeTruthy();
+    });
+  });
 });
