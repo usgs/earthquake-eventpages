49f3647507ceb811f3f2910956ce6fa34849d869
ehunter-usgs <ehunter@usgs.gov>
2018-09-27 14:42:45 -0600

    Small fixes for ground-failure

--- src/app/ground-failure/ground-failure/ground-failure.component.ts
+++ src/app/ground-failure/ground-failure/ground-failure.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { EventService } from '@core/event.service';
 
@@ -6,7 +6,6 @@ import { EventService } from '@core/event.service';
  * Display ground-failure product page
  */
 @Component({
-  encapsulation: ViewEncapsulation.None,
   selector: 'ground-failure',
   styleUrls: ['./ground-failure.component.scss'],
   templateUrl: './ground-failure.component.html'
