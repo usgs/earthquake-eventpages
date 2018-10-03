b7ae765cc3b61b0863720b84377c20a8b14039c2
jdbrown-usgs <jdbrown@usgs.gov>
2018-10-03 09:18:18 -0600

    created pipe

--- src/app/shared/has-product.pipe.ts
+++ src/app/shared/has-product.pipe.ts
@@ -5,6 +5,18 @@ import { Event } from '../event';
 @Pipe({
   name: 'sharedHasProduct'
 })
+
+/**
+ * Checks to see if event has a given product
+ *
+ * @param event
+ *      The given event
+ * @param type
+ *      The type of product to check for
+ *
+ * @return boolean
+ *    true or false
+ */
 export class HasProductPipe implements PipeTransform {
   transform(event: Event, type: string): any {
     return event.hasProducts(type);
