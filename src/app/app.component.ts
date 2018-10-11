c83e61edb6ed63d805d19dd02549b92190153f8f
jdbrown-usgs <jdbrown@usgs.gov>
2018-10-11 12:53:50 -0600

    cleaned up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 
