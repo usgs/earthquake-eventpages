0a5731a38a6f57df2a3734255b018cb4dd3498cc
jdbrown-usgs <jdbrown@usgs.gov>
2018-10-16 14:30:23 -0600

    can now set what a mock pipe returns

--- src/app/mock-pipe.ts
+++ src/app/mock-pipe.ts
@@ -2,12 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 // Mock a pipe
 // Credit: https://github.com/cnunciato/ng2-mock-component/issues/14
-export function MockPipe(name: string): any {
+export function MockPipe(name: string, transform?): any {
+  const defaultTransform = (input: any, ...args: any[]): any => {
+    return input;
+  };
+
   @Pipe({ name })
   class MockPipeImpl implements PipeTransform {
-    transform(input: any, ...args: any[]): any {
-      return input;
-    }
+    transform = transform || defaultTransform;
   }
 
   return MockPipeImpl;
