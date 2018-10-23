318b6dc9d14a13dd29a0898ee38f9cc5d71fcfa5
jdbrown-usgs <jdbrown@usgs.gov>
2018-10-23 09:20:32 -0600

    updated base href

--- hooks/01-base-href-hook.sh
+++ hooks/01-base-href-hook.sh
@@ -10,12 +10,9 @@ fi
 
 # Update bundles to be BASE_HREF aware
 SED_SANITIZED_HREF=${BASE_HREF//\//\\/};
-sed "s/BASE_HREF/${SED_SANITIZED_HREF}/" \
-  ${DOCUMENT_ROOT}/BASE_HREF/index.html \
-  > ${DOCUMENT_ROOT}/BASE_HREF/index-base-href.html;
-
-mv ${DOCUMENT_ROOT}/BASE_HREF/index-base-href.html \
-  ${DOCUMENT_ROOT}/BASE_HREF/index.html
+sed --in-place
+  "s/BASE_HREF/${SED_SANITIZED_HREF}/" \
+  ${DOCUMENT_ROOT}/BASE_HREF/index.html;
 
 mkdir -p ${DOCUMENT_ROOT}/$(dirname ${BASE_HREF});
 ln -sf ${DOCUMENT_ROOT}/BASE_HREF ${DOCUMENT_ROOT}/${BASE_HREF};
