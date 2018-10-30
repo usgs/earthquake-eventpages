d9763472acbffba42d200e23ad3c1b7b5a8635d9
jdbrown-usgs <jdbrown@usgs.gov>
2018-10-30 10:46:15 -0600

    updated to use try catch

--- src/app/ground-failure/pending.pipe.ts
+++ src/app/ground-failure/pending.pipe.ts
@@ -5,25 +5,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PendingPipe implements PipeTransform {
   /**
-   * Checks to see if landslides and liquifaction are both in a pending state.
+   * Checks to see if landslides and liquifaction are in a pending state.
    * @param product
    *    Ground failure product
    *
    * @return boolean
-   *
+   *    True = None in pending state
+   *    False = One is in a pending state.
    */
   transform(product: any): any {
     if (!product || !product.properties) {
       return null;
     }
 
-    if (
-      product.properties['landslide-alert'].toLowerCase() !== 'pending' &&
-      product.properties['liquefaction-alert'].toLowerCase() !== 'pending'
-    ) {
-      return true;
-    } else {
-      return false;
+    let pending = false;
+
+    try {
+      pending =
+        product.properties['landslide-alert'].toLowerCase() !== 'pending' &&
+        product.properties['liquefaction-alert'].toLowerCase() !== 'pending';
+    } catch (e) {
+      pending = false;
     }
+
+    return pending;
   }
 }
