693176bf28508ce7efd8de431b209c6588cd3c61
dslosky-usgs <dslosky@usgs.gov>
2018-11-14 09:38:03 -0700

    Rearrange shakemap module imports

--- src/app/shakemap/shakemap.module.ts
+++ src/app/shakemap/shakemap.module.ts
@@ -19,6 +19,7 @@ import { SharedModule } from '@shared/shared.module';
 import { IntensityComponent } from './intensity/intensity.component';
 import { InputComponent } from './metadata/input/input.component';
 import { MetadataComponent } from './metadata/metadata.component';
+import { MultiGmpeComponent } from './metadata/multigmpe/multigmpe.component';
 import { OutputComponent } from './metadata/output/output.component';
 import { ProcessingComponent } from './metadata/processing/processing.component';
 import { PgaComponent } from './pga/pga.component';
@@ -27,21 +28,20 @@ import { PsaComponent } from './psa/psa.component';
 import { ShakemapRoutingModule } from './shakemap-routing.module';
 import { ShakemapComponent } from './shakemap/shakemap.component';
 import { StationListComponent } from './station-list/station-list.component';
-import { MultiGmpeComponent } from './metadata/multigmpe/multigmpe.component';
 
 @NgModule({
   declarations: [
     ShakemapComponent,
     StationListComponent,
     MetadataComponent,
+    MultiGmpeComponent,
     InputComponent,
     OutputComponent,
     ProcessingComponent,
     IntensityComponent,
     PgaComponent,
     PgvComponent,
-    PsaComponent,
-    MultiGmpeComponent
+    PsaComponent
   ],
   exports: [StationListComponent, MetadataComponent],
   imports: [
