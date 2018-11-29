028866063f1195c170bdf16c2ce5ed60457cafe1
jdbrown-usgs <jdbrown@usgs.gov>
2018-11-29 15:56:58 -0700

    created basic setup for everyone to build from.

--- src/app/shake-alert/shake-alert/shake-alert.component.ts
+++ src/app/shake-alert/shake-alert/shake-alert.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventService } from '@core/event.service';
+
 @Component({
   selector: 'shake-alert',
   styleUrls: ['./shake-alert.component.scss'],
   templateUrl: './shake-alert.component.html'
 })
 export class ShakeAlertComponent implements OnInit {
-  constructor() {}
+  constructor(public eventService: EventService) {}
 
   ngOnInit() {}
 }
