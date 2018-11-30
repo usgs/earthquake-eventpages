65fe5808b6853dcd075f415041a63f20a2b996a1
jdbrown-usgs <jdbrown@usgs.gov>
2018-11-30 07:24:38 -0700

    removed event service that is not being used

--- src/app/shake-alert/shake-alert/shake-alert.component.ts
+++ src/app/shake-alert/shake-alert/shake-alert.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { EventService } from '@core/event.service';
-
 @Component({
   selector: 'shake-alert',
   styleUrls: ['./shake-alert.component.scss'],
   templateUrl: './shake-alert.component.html'
 })
 export class ShakeAlertComponent implements OnInit {
-  constructor(public eventService: EventService) {}
+  constructor() {}
 
   ngOnInit() {}
 }
