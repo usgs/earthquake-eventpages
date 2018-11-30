1f672cd81321e848a85e3a0e38c2962df6a19843
Chris Wilson <cswilson@usgs.gov>
2018-11-30 11:31:34 -0700

    added shake-alert-pending component

--- src/app/shake-alert/shake-alert.module.ts
+++ src/app/shake-alert/shake-alert.module.ts
@@ -2,16 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '@shared/shared.module';
 import { ProductPageModule } from '../product-page/product-page.module';
 
-import { SharedModule } from '@shared/shared.module';
 import { ShakeAlertService } from './shake-alert.service';
 import { ShakeAlertComponent } from './shake-alert/shake-alert.component';
-import { ShakeAlertRoutingModule } from './shake-alert-routing.module';
 import { ShakeAlertMissedComponent } from './shake-alert/shake-alert-missed/shake-alert-missed.component';
+import { ShakeAlertPendingComponent } from './shake-alert/shake-alert-pending/shake-alert-pending.component';
+import { ShakeAlertRoutingModule } from './shake-alert-routing.module';
 
 @NgModule({
-  declarations: [ShakeAlertComponent, ShakeAlertMissedComponent],
+  declarations: [
+    ShakeAlertComponent,
+    ShakeAlertMissedComponent,
+    ShakeAlertPendingComponent
+  ],
   exports: [],
   imports: [
     CommonModule,
---
+++ src/app/shake-alert/shake-alert/shake-alert-pending/shake-alert-pending.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * Shake alert pending presentational component
+ */
+@Component({
+  selector: 'shake-alert-pending',
+  styleUrls: ['./shake-alert-pending.component.scss'],
+  templateUrl: './shake-alert-pending.component.html'
+})
+export class ShakeAlertPendingComponent { }
