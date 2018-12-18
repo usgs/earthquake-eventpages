56da2aaf2895890ff74318d39911dc53b34f60ae
Eddie Hunter <ehunter@usgs.gov>
2018-12-18 11:07:14 -0700

    Add links for Waveform and Tell Us pages

--- src/app/waveforms/waveforms.module.ts
+++ src/app/waveforms/waveforms.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@shared/shared.module';
 import { WaveformService } from './waveform.service';
 import { WaveformsRoutingModule } from './waveforms-routing.module';
 import { WaveformsComponent } from './waveforms/waveforms.component';
 
 @NgModule({
   declarations: [WaveformsComponent],
-  imports: [CommonModule, WaveformsRoutingModule],
+  imports: [CommonModule, SharedModule, WaveformsRoutingModule],
   providers: [WaveformService]
 })
 export class WaveformsModule {}
