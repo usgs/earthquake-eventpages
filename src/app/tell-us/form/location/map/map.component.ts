ebfbf0fd2e41d0d1fd1f11e4c3adb9da04542d22
Eric Martinez <emartinez@usgs.gov>
2019-02-13 07:36:48 -0700

    Added map component.

---
+++ src/app/tell-us/form/location/map/map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tell-us-form-location-map',
+  styleUrls: ['./map.component.scss'],
+  templateUrl: './map.component.html'
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
