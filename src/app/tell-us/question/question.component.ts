e948ed70f3c8275f03b756f135dd980de3159b42
Eric Martinez <emartinez@usgs.gov>
2019-02-13 07:03:47 -0700

    using two-way data bindings for question component.

--- src/app/tell-us/question/question.component.ts
+++ src/app/tell-us/question/question.component.ts
@@ -1,41 +1,22 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Output,
-  Input
-} from '@angular/core';
-import {
-  MatSelectionListChange,
-  MatRadioChange,
-  MatListOption
-} from '@angular/material';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs';
-
-
-/**
- * Interface for user to enter data under any of the 'other' radio buttons
- */
-interface OtherValueEvent {
-  type: 'other';
-  value: any;
-}
+import { TellUsText } from '../form-language/tell-us-text';
 
 /**
  * The question form component
  *
- * @param change
- *     Behaviorsubject, emits changes to form
  * @param label
  *     The form label
+ * @param labels
+ *     Translation strings mapping
+ * @param model
+ *     Model to render
  * @param multiSelect
  *     Multiselect options on form
  * @param name
  *     The name input on form
  * @param options
  *     Other form options input
- * @param value
- *     The form input value
  *
  */
 @Component({
@@ -45,52 +26,18 @@ interface OtherValueEvent {
   templateUrl: './question.component.html'
 })
 export class QuestionComponent {
-  @Output()
-  change = new BehaviorSubject<any>(null);
-
   @Input()
   label: string;
+  @Input()
+  labels: TellUsText;
+  @Input()
+  model: any;
   @Input()
   multiSelect = false;
   @Input()
   name: string;
   @Input()
   options: Array<any>;
-  // if there is an "other" option, the user specified value
-  otherValue: any;
-  @Input()
-  value: any;
-
-  /**
-   * Convert UI value changes to answer values
-   *
-   * @param changeEvent
-   *     UI selection change.
-   */
-  onChange(
-    changeEvent: MatRadioChange | MatSelectionListChange | OtherValueEvent
-  ) {
-    if (changeEvent instanceof MatRadioChange) {
-      // single select
-      this.value = changeEvent.value;
-    } else if (changeEvent instanceof MatSelectionListChange) {
-      // multi select
-      this.value = changeEvent.source.selectedOptions.selected.map(
-        (option: MatListOption) => {
-          return option.value;
-        }
-      );
-    } else if (changeEvent) {
-      this.otherValue = changeEvent.value;
-    }
-
-    const next = {};
-    next[this.name] = this.value;
-    if (this.otherValue !== undefined) {
-      next[this.name + '_Other'] = this.otherValue;
-    }
-    this.change.next(next);
-  }
 
   /**
    * Checks for changes to data by index
@@ -100,7 +47,7 @@ export class QuestionComponent {
    * @param item
    *    question item
    */
-  trackByIndex (index, item) {
+  trackByIndex(index, item) {
     return index;
   }
 }
