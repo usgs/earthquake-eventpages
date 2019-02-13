58f41abc6f520e388844a5edde56682be762fecf
Eric Martinez <emartinez@usgs.gov>
2019-02-13 07:05:27 -0700

    Update module with new classes

--- src/app/tell-us/tell-us.module.ts
+++ src/app/tell-us/tell-us.module.ts
@@ -17,13 +17,22 @@ import { SharedModule } from '@shared/shared.module';
 import { FieldsetComponent } from './fieldset/fieldset.component';
 import { FormComponent } from './form/form.component';
 import { FormLanguageService } from './form-language.service';
-import { LocationComponent } from './location/location.component';
+import { LocationComponent } from './form/location/location.component';
 import { PrivacyStatementComponent } from './privacy-statement/privacy-statement.component';
 import { QuestionComponent } from './question/question.component';
 import { SuccessViewComponent } from './success-view/success-view.component';
 import { TellUsComponent } from './tell-us/tell-us.component';
 import { TellUsRoutingModule } from './tell-us-routing.module';
 import { LocationViewModule } from 'hazdev-ng-location-view';
+import { LanguagePipe } from './language.pipe';
+import { ResponseComponent } from './response/response.component';
+import { MetadataComponent } from './metadata/metadata.component';
+import { MapDialogComponent } from './location/map/map.component';
+import { CoordinateMarkerPipe } from './location/coordinate-marker.pipe';
+import { FeltComponent } from './form/felt/felt.component';
+import { OptionalComponent } from './form/optional/optional.component';
+import { ContactComponent } from './form/contact/contact.component';
+import { SubmitComponent } from './form/submit/submit.component';
 
 @NgModule({
   declarations: [
@@ -33,9 +42,18 @@ import { LocationViewModule } from 'hazdev-ng-location-view';
     QuestionComponent,
     SuccessViewComponent,
     TellUsComponent,
-    PrivacyStatementComponent
+    PrivacyStatementComponent,
+    LanguagePipe,
+    ResponseComponent,
+    MetadataComponent,
+    MapDialogComponent,
+    CoordinateMarkerPipe,
+    FeltComponent,
+    OptionalComponent,
+    ContactComponent,
+    SubmitComponent
   ],
-  entryComponents: [FormComponent],
+  entryComponents: [MapDialogComponent],
   exports: [FormComponent, TellUsComponent],
   imports: [
     CommonModule,
