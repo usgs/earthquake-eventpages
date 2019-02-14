415ba79a26da8a35604a0f1b81e2eb5754fda2f0
jdbrown-usgs <jdbrown@usgs.gov>
2019-02-14 15:53:00 -0700

    cleaned up test

--- src/app/tell-us/form/location/location.component.spec.ts
+++ src/app/tell-us/form/location/location.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng2-mock-component';
 
 import { LocationComponent } from './location.component';
+import { CoordinatesService } from 'hazdev-ng-location-view';
+import { FormatterService } from '@core/formatter.service';
 
 describe('LocationComponent', () => {
   let component: LocationComponent;
@@ -8,9 +11,39 @@ describe('LocationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LocationComponent,
+        MockComponent({
+          inputs: ['legend'],
+          selector: 'tell-us-fieldset'
+        }),
+        MockComponent({
+          inputs: ['diameter'],
+          selector: 'mat-spinner'
+        }),
+        MockComponent({
+          selector: 'mat-icon'
+        }),
+        MockComponent({
+          inputs: ['ngModel'],
+          selector: 'input'
+        }),
+        MockComponent({
+          selector: 'mat-form-field'
+        }),
+        MockComponent({
+          selector: 'mat-expansion-panel-header'
+        }),
+        MockComponent({
+          inputs: ['event', 'labels', 'feltReport'],
+          selector: 'tell-us-form-location-map'
+        }),
+        MockComponent({
+          selector: 'mat-expansion-panel'
+        })
+      ],
+      providers: [CoordinatesService, FormatterService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
