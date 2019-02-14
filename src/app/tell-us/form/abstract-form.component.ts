f69c9e7b90a0041828b883715fb5b4a95f82fdce
jdbrown-usgs <jdbrown@usgs.gov>
2019-02-14 15:56:06 -0700

    updated

--- src/app/tell-us/form/abstract-form.component.ts
+++ src/app/tell-us/form/abstract-form.component.ts
@@ -2,11 +2,12 @@ import { Input } from '@angular/core';
 import { Event } from 'app/event';
 import { FeltReport } from '../felt-report';
 import { TellUsText } from '../form-language/tell-us-text';
+import * as LANGUAGE_EN from '../form-language/en.json';
 
 export abstract class AbstractForm {
   @Input() event: Event;
   @Input() feltReport = new FeltReport();
-  @Input() labels: TellUsText;
+  @Input() labels = LANGUAGE_EN;
 
   get hasEvent(): boolean {
     if (this.event && this.event.id) {
