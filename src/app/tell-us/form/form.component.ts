c4eae0d8344a3647eed7c2fe7a51551659c24a01
jdbrown-usgs <jdbrown@usgs.gov>
2019-02-14 15:55:07 -0700

    removed imports that are no longer used

--- src/app/tell-us/form/form.component.ts
+++ src/app/tell-us/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 import { Event } from '../../event';
 import { TellUsText } from '../form-language/tell-us-text';
+
 import { AbstractForm } from './abstract-form.component';
 
 /**
