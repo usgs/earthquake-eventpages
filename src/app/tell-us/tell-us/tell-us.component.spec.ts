674e5451e92c53e82066917e1958e9f1218b56da
jdbrown-usgs <jdbrown@usgs.gov>
2019-02-19 08:31:22 -0700

    commented out imports that are not used at this time

--- src/app/tell-us/tell-us/tell-us.component.spec.ts
+++ src/app/tell-us/tell-us/tell-us.component.spec.ts
@@ -1,28 +1,30 @@
-import { Location } from '@angular/common';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import {
-  MatButtonModule,
-  MatDialogModule,
-  MatDialog,
-  MatExpansionModule,
-  MatSelectModule,
-  MatFormFieldModule
-} from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+// import { Location } from '@angular/common';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { FormsModule } from '@angular/forms';
+// import {
+//   MatButtonModule,
+//   MatDialogModule,
+//   MatDialog,
+//   MatExpansionModule,
+//   MatSelectModule,
+//   MatFormFieldModule
+// } from '@angular/material';
+// import
+// { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserDynamicTestingModule }
+// from '@angular/platform-browser-dynamic/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
 
-import { MockPipe } from '../../mock-pipe';
-import { MockComponent } from 'ng2-mock-component';
-import { of } from 'rxjs/observable/of';
-import { Subject } from 'rxjs/Subject';
+// import { MockPipe } from '../../mock-pipe';
+// import { MockComponent } from 'ng2-mock-component';
+// import { of } from 'rxjs/observable/of';
+// import { Subject } from 'rxjs/Subject';
 
-import { EventService } from '@core/event.service';
-import { Event } from '../../event';
-import { FormLanguageService } from '../form-language.service';
-import { FormComponent } from '../form/form.component';
-import { TellUsComponent } from './tell-us.component';
+// import { EventService } from '@core/event.service';
+// import { Event } from '../../event';
+// import { FormLanguageService } from '../form-language.service';
+// import { FormComponent } from '../form/form.component';
+// import { TellUsComponent } from './tell-us.component';
 
 describe('TellUsComponent', () => {
   // let component: TellUsComponent;
