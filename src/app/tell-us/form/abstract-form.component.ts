b5bb42bd2ef72ac9bd6cba601efa05c15050ea6d
Chris Wilson <cswilson@usgs.gov>
2019-02-20 09:02:15 -0700

    put OMB number in flexbox above/right of the privacy expansion panel

--- src/app/tell-us/form/abstract-form.component.ts
+++ src/app/tell-us/form/abstract-form.component.ts
@@ -1,12 +1,14 @@
 import { Input } from '@angular/core';
-import { Event } from 'app/event';
+
 import { FeltReport } from '../felt-report';
 import * as LANGUAGE_EN from '../form-language/en.json';
+import { TellUsText } from '../form-language/tell-us-text';
+import { Event } from 'app/event';
 
 export abstract class AbstractForm {
   @Input() event: Event;
   @Input() feltReport = new FeltReport();
-  @Input() labels = LANGUAGE_EN;
+  @Input() labels: TellUsText = (LANGUAGE_EN as unknown) as TellUsText;
 
   get hasEvent(): boolean {
     if (this.event && this.event.id) {
