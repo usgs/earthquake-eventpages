82600b1388069aeea5f37a2c6aab592627da388a
Eddie Hunter <ehunter@usgs.gov>
2019-02-22 10:01:56 -0700

    Clean up test in question component

--- src/app/tell-us/question/question.component.spec.ts
+++ src/app/tell-us/question/question.component.spec.ts
@@ -14,7 +14,7 @@ import { MockComponent } from 'ng2-mock-component';
 
 import { QuestionComponent } from './question.component';
 
-describe('QuestionComponent', () => {
+fdescribe('QuestionComponent', () => {
   let component: QuestionComponent;
   let fixture: ComponentFixture<QuestionComponent>;
 
@@ -23,9 +23,22 @@ describe('QuestionComponent', () => {
       declarations: [
         QuestionComponent,
 
-        MockComponent({ selector: 'tell-us-fieldset', inputs: ['legend'] })
-      ],
-      imports: [FormsModule, MatFormFieldModule, MatListModule, MatRadioModule]
+        MockComponent({ selector: 'tell-us-fieldset', inputs: ['legend'] }),
+        MockComponent({ selector: 'mat-selection-list', inputs: ['ngModel'] }),
+        MockComponent({
+          selector: 'mat-list-option',
+          inputs: ['checkboxPosition', 'selected', 'value']
+        }),
+        MockComponent({ selector: 'mat-radio-group', inputs: ['ngModel'] }),
+        MockComponent({ selector: 'mat-list' }),
+        MockComponent({ selector: 'mat-list-item' }),
+        MockComponent({ selector: 'mat-radio-button', inputs: ['value'] }),
+        MockComponent({ selector: 'mat-form-field' }),
+        MockComponent({
+          selector: 'input',
+          inputs: ['disabled', 'ngModel', 'placeholder']
+        })
+      ]
     }).compileComponents();
   }));
 
@@ -35,60 +48,15 @@ describe('QuestionComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // describe('onChange', () => {
-  //   it('gets value for MatRadioChange', () => {
-  //     const change = new MatRadioChange(null, 'test value');
-  //     component.name = 'test name';
-  //     spyOn(component.change, 'next');
-  //     component.onChange(change);
-  //     expect(component.change.next).toHaveBeenCalledWith({
-  //       'test name': 'test value'
-  //     });
-  //   });
-
-  //   it('gets values for MatSelectionListChange', () => {
-  //     const source = new MatSelectionList(null, '-1');
-  //     const option1 = new MatListOption(null, null, source);
-  //     option1.value = 'test value 1';
-  //     const option2 = new MatListOption(null, null, source);
-  //     option2.value = 'test value 2';
-  //     const change = new MatSelectionListChange(source, null);
-  //     spyOnProperty(source.selectedOptions, 'selected', 'get').
-  //     and.returnValue([
-  //       option1,
-  //       option2
-  //     ]);
-  //     component.name = 'test name';
-  //     spyOn(component.change, 'next');
-  //     component.onChange(change);
-  //     expect(component.change.next).toHaveBeenCalledWith({
-  //       'test name': ['test value 1', 'test value 2']
-  //     });
-  //   });
-
-  //   it('gets other value for OtherValueChange', () => {
-  //     component.name = 'test name';
-  //     component.value = 'other';
-  //     spyOn(component.change, 'next');
-  //     component.onChange({ type: 'other', value: 'test other' });
-  //     expect(component.change.next).toHaveBeenCalledWith({
-  //       'test name': 'other',
-  //       'test name_Other': 'test other'
-  //     });
-  //   });
-
-  //   it('does not change value otherwise', () => {
-  //     component.value = 'test value';
-  //     spyOn(component.change, 'next');
-  //     component.name = 'test name';
-  //     component.onChange(null);
-  //     expect(component.change.next).toHaveBeenCalledWith({
-  //       'test name': 'test value'
-  //     });
+  // describe('trackByIndex', () => {
+  //   it('returns the index', () => {
+  //     const index = 1;
+  //     const result = component.trackByIndex(index, null);
+  //     expect(index).toBe(result);
   //   });
   // });
 });
