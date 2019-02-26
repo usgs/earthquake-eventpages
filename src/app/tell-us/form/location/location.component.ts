b0160a2eb59f7694449fbb7dc47ebbcda91744d1
Eric Martinez <emartinez@usgs.gov>
2019-02-25 18:12:19 -0700

    Using geoserve for async operations.

--- src/app/tell-us/form/location/location.component.ts
+++ src/app/tell-us/form/location/location.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { AbstractForm } from '../abstract-form.component';
-// import { CoordinatesService } from 'hazdev-ng-location-view';
-import { FormatterService } from '@core/formatter.service';
 import { MatSnackBar } from '@angular/material';
 
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { GeolocateService } from 'app/tell-us/geolocate.service';
+import GeoService from '@shared/geo.service';
+
+import { AbstractForm } from '../abstract-form.component';
 
 @Component({
   selector: 'tell-us-form-location',
@@ -16,160 +11,11 @@ import { GeolocateService } from 'app/tell-us/geolocate.service';
   templateUrl: './location.component.html'
 })
 export class LocationComponent extends AbstractForm {
-  GEOCODE_URL =
-    'https://geocode.arcgis.com/arcgis/rest/services/' +
-    'World/GeocodeServer/find';
-
   geolocating = false;
 
-  constructor(
-    // public coordinateService: CoordinatesService,
-    public formatter: FormatterService,
-    public geolocator: GeolocateService,
-    public http: HttpClient,
-    public snackBar: MatSnackBar
-  ) {
+  constructor(public geoService: GeoService, public snackBar: MatSnackBar) {
     super();
   }
-
-  /**
-   * Builds the request URL for the ArcGIS geocode
-   *
-   * @param location
-   *    An address or geographic coordinate string
-   */
-  buildUrl(location: string): string {
-    return this.GEOCODE_URL + '?' + `f=json` + `&text=${location}`;
-  }
-
-  /**
-   * Geocode the input string using the ArcGIS geocode service
-   *
-   * @param location
-   *     An address or geographic coordinate string
-   */
-  geocode(location: string) {
-    if (!location) {
-      return;
-    }
-
-    this.geolocating = false;
-    const url = this.buildUrl(location);
-
-    // make a geocode request
-    this.http
-      .get<any>(url)
-      .pipe(catchError(this.handleError('getLocation', { locations: null })))
-      .subscribe(response => {
-        if (response.locations && response.locations.length !== 0) {
-          this.onGeocodeSuccess(response.locations[0]);
-        } else {
-          this.onGeocodeError();
-        }
-      });
-  }
-
-  // /**
-  //  * Geolocate, get the users current location
-  //  */
-  // geolocate() {
-  //   try {
-  //     this.geolocating = true;
-  //     navigator.geolocation.getCurrentPosition(
-  //       position => this.onGeolocateResult(position),
-  //       error => this.onGeolocateError(error)
-  //     );
-  //   } catch (e) {
-  //     console.log(`Failed geolocation: ${e}`);
-  //   }
-  // }
-
-  /**
-   * Handle http error
-   *
-   * @param action
-   * @param result
-   */
-  handleError<T>(action: string, result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    };
-  }
-
-  /**
-   * Handles a geocode response with no results
-   */
-  onGeocodeError() {
-    // Display error message. Use Snackbar.
-    this.openSnackBar('No results. Please search again.', null, 3000);
-  }
-
-  /**
-   * Handles a geocode response with at least one result
-   *
-   * @param response
-   *      A geocode response from the ArcGIS geocode api
-   *
-   */
-  onGeocodeSuccess(response: any) {
-    // pull lat/lng/address from geocode response
-    const latitude = response.feature.geometry.y;
-    const longitude = response.feature.geometry.x;
-    const address = response.name;
-
-    // update location from FeltReport object
-    this.feltReport.location = {
-      address: address,
-      latitude: latitude,
-      longitude: longitude
-    };
-
-    // TODO, create success message in snackbar??
-  }
-
-  // onGeolocateError(error: PositionError) {
-  //   this.geolocating = false;
-
-  //   // TODO :: Let user know about error. Snackbar?
-  //   // error.code === 0 --> unknown error
-  //   // error.code === 1 --> permission denied
-  //   // error.code === 2 --> unavailable
-  //   // error.code === 3 --> timeout exceeded
-  //   // Note: Timeout can occur if permission not given/denied in time
-  //   //
-  //   // error.message seems okay to show user
-  //   console.log('A geolocating error occurred', error);
-  // }
-
-  // onGeolocateResult(position: Position) {
-  //   if (!this.geolocating) {
-  //     return;
-  //   }
-
-  //   const confidence = this.coordinateService.computeFromGeolocate(
-  //     position.coords.accuracy
-  //   );
-  //   const latitude = this.coordinateService.roundLocation(
-  //     +position.coords.latitude,
-  //     confidence
-  //   );
-  //   const longitude = this.coordinateService.roundLocation(
-  //     +position.coords.longitude,
-  //     confidence
-  //   );
-
-  //   this.feltReport.location = {
-  //     address: this.formatter.location(latitude, longitude, confidence),
-  //     latitude,
-  //     longitude
-  //   };
-
-  //   // Artificially force spinner to show for a minimum time. This may extend
-  //   // the time slightly, but otherwise fast responses might give janky flicker
-  //   // effect
-  //   setTimeout(_ => (this.geolocating = false), 500);
-  // }
 
   /**
    * A generic method that displays a snackbar with the provided information
