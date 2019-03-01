eb7ebc798471225170f45a821acd1a8aa228b0e6
Eric Martinez <emartinez@usgs.gov>
2019-02-28 17:24:00 -0700

    Some minor refactoring. Added second event id for some products.

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,19 +1,52 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  // Includes these product types:
+  // - dyfi
+  // - finite-fault
+  // - general-link
+  // - general-text
+  // - geoserve
+  // - ground-failure
+  // - impact-link
+  // - impact-text
+  // - losspager
+  // - moment-tensor
+  // - oaf
+  // - origin
+  // - phase-data
+  // - poster
+  // - scitech-link
+  // - shakemap
+  EVENT_ID_1 = 'ak20419010';
+
+  // Includes these product types
+  // - dyfi
+  // - focal-mechanism             <-- Notable
+  // - geoserve
+  // - nearby-cities               <-- Notable
+  // - origin
+  // - phase-data
+  // - shakemap
+  EVENT_ID_2 = 'ci38474368';
+
   getMainPageHeaderTitle() {
-    return element(by.css('main > header > h1')).getText();
+    return this.select('main > header > h1').getText();
   }
 
   getPageSectionTitle() {
-    return element(by.css('.product-page-header > h3')).getText();
+    return this.select('.product-page-header > h2').getText();
   }
 
   getUnknownPageHeaderTitle() {
-    return element(by.css('h1')).getText();
+    return this.select('h1').getText();
   }
 
   navigateTo(path = '/') {
     return browser.get(path);
+  }
+
+  select(selector: string) {
+    return element(by.css(selector));
   }
 }
