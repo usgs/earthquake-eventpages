af7e108e8ccbce0a6e1a595f79b1a2e2a817571c
Eddie Hunter <ehunter@usgs.gov>
2019-04-17 10:35:12 -0600

    Add milliseconds to times on shake-alert page

--- src/app/shared/date-time.pipe.ts
+++ src/app/shared/date-time.pipe.ts
@@ -14,11 +14,15 @@ export class DateTimePipe implements PipeTransform {
    * @param time
    *     Time to be transformed into a timestamp. Must be a format readable
    *     by the Date JavaScript object
+   * @param minutesOffset {Number} Optional, default 0
+   *     UTC offset in minutes. 0 for UTC
+   * @param includeMilliseconds {Boolean} Optional, default false
+   *     Whether to output milliseconds
    *
    * @return {string}
    *     formatted date/time string. Ex: '2018-08-31 23:05:43 (UTC)'
    */
-  transform(time: any): string {
+  transform(time: any, minutesOffset = 0, includeMilliseconds = false): string {
     let date = new Date(time);
 
     // check for valid date
@@ -26,6 +30,6 @@ export class DateTimePipe implements PipeTransform {
       date = null;
     }
 
-    return this.formatter.dateTime(date);
+    return this.formatter.dateTime(date, minutesOffset, includeMilliseconds);
   }
 }
