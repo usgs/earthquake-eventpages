4cd5da5f7e08bb5683397794a00a38170e23d6de
Jeremy Fee <jmfee@usgs.gov>
2020-04-22 18:30:08 -0600

    Use node10 ng-cli-e2e image

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -35,7 +35,8 @@ node {
 
 
   // Used to run linting, unit tests, coverage, and e2e within this container
-  def TESTER_IMAGE = "${DEVOPS_REGISTRY}/usgs/ng-cli-e2e"
+  // def TESTER_IMAGE = "${DEVOPS_REGISTRY}/usgs/ng-cli-e2e"
+  def TESTER_IMAGE = "trion/ng-cli-e2e:node10"
 
   // Queue up tasks that can be run in parallel
   def SECURITY_CHECKS = [:]
