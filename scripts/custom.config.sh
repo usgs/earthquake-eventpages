088e001d63fba775fd52e9d0c0803631971a24dc
dslosky-usgs <dslosky@usgs.gov>
2021-02-09 08:43:28 -0700

    Updates for integration/staging

---
+++ scripts/custom.config.sh
@@ -0,0 +1,6 @@
+export ASSETS_FILE='old_assets.txt';
+export BASE_HREF='earthquakes/map';
+export SITE_URL="${SITE_URL_PREFIX}earthquake${SITE_URL_SUFFIX}";
+export SERVICE_MAP=(
+  "/${BASE_HREF}":'web'
+);
