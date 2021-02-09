9e114012af40ae65dde2880eff347b72d6078cd2
dslosky-usgs <dslosky@usgs.gov>
2021-02-09 15:00:53 -0700

    updating metadata.json when image builds

---
+++ scripts/update-metadata.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+VERSION=$(git describe || echo 'unknown');
+BRANCH=${CI_COMMIT_REF_SLUG:-"unknown"};
+COMMIT=${CI_COMMIT_SHA:-"unknown"};
+
+if [[ "${BRANCH}" == 'master' || "${BRANCH}" == 'production' ]]; then
+  IMAGE="latest";
+else
+  IMAGE=${CI_COMMIT_REF_SLUG:-"unknown"};
+fi
+
+cat <<-EO_METADATA > metadata.json
+{
+  "version": "${VERSION}",
+  "branch": "${BRANCH}",
+  "commit": "${COMMIT}",
+  "image": "${IMAGE}"
+}
+EO_METADATA
