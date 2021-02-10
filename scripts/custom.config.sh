aee8359a4f718bd2acf33aa8325b23b94e559f6d
dslosky-usgs <dslosky@usgs.gov>
2021-02-10 14:33:59 -0700

    Fix default base_href

--- scripts/custom.config.sh
+++ scripts/custom.config.sh
@@ -1,5 +1,5 @@
 export ASSETS_FILE='old_assets.txt';
-export BASE_HREF=${BASE_HREF:-'earthquakes/eventpages'};
+export BASE_HREF=${BASE_HREF:-'earthquakes/eventpage'};
 export EVENT_TYPE=${EVENT_TYPE:-'actual'};
 export SITE_URL="${SITE_URL_PREFIX}earthquake${SITE_URL_SUFFIX}";
 export SERVICE_MAP=(
